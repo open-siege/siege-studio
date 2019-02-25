5bbac603a930754e5e52c2336285d362f823eedd
Matthew Rindel <matthew@sobantu.co.za>
2019-02-25 09:52:50 +0200

    * added support for Colony Wars Vengeance resource files
    * removed old code to read the FRONT.OVL file

--- extractRsc.py
+++ extractRsc.py
@@ -7,37 +7,42 @@ import glob
 from collections import namedtuple
 
 
+def readFiles(numFiles, infoFmt, offset):
+	files = []
+        for i in range(numFiles):
+            rawValues = struct.unpack_from(infoFmt, rawData, offset)
+            fileName = rawValues[0]
+	    fileOffset = rawValues[1]
+            fileName =  fileName.split("\0")[0]
+            offset += struct.calcsize(infoFmt)
+            files.append((fileName, fileOffset))
+	return files
+
 importFilenames = sys.argv[1:]
 
 for importFilename in importFilenames:
 
     print "reading " + importFilename
     try:
-        with open("FRONT.OVL", "rb") as input_fd:
-            rawData = input_fd.read()
-
-        frontLen = len(rawData)
         with open(importFilename, "rb") as input_fd:
             rawData = input_fd.read()
-        # then map them for conversation later
+
         offset = 0
         (numFiles, ) = struct.unpack_from("<L", rawData, offset)
         offset += struct.calcsize("<L")
-        infoFmt = "<2l4h"
-        files = []
-        for i in range(numFiles):
-            filelength = rawData.index("\0", offset) - offset  
-              
-            fmt = "<" + str(16) + "s"
-            (fileName, ) = struct.unpack_from(fmt, rawData, offset)
-            fileName =  fileName.split("\0")[0]
-            offset += struct.calcsize(fmt)
-            fileinfo = struct.unpack_from(infoFmt, rawData, offset)
-            offset += struct.calcsize(infoFmt)
-            files.append((fileName, fileinfo[0]))
+        infoFmt = "<16sl"
+	v1infoFmt = "<16s2l4h"
+	
+        files = readFiles(numFiles, v1infoFmt, offset)
+	
+	# this would be true for CW Vengeance of the resource file
+	if "." not in files[1][0]:
+		files = readFiles(numFiles, infoFmt, offset)
+	
         
         for index, file in enumerate(files):
             filename, fileOffset = file
+	    print "extracting " + filename
             nextFileOffset = len(rawData)
             if index + 1 < numFiles:
                 nextFilename, nextFileOffset = files[index + 1]
