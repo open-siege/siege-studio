3b41e5578064f3c6867cf5c6b4817ed30a801115
Matthew Rindel <matthew@sobantu.co.za>
2019-03-05 08:06:04 +0200

    * updated extractDyn to view the names of each file inside of the archive

--- extractDyn.py
+++ extractDyn.py
@@ -17,7 +17,7 @@ for importFilename in importFilenames:
         destDir = importFilename.replace(".dyn", "").replace(".DYN", "")
         offset = 0
         headerFmt = "<20s"
-        filenameFmt = "<h13sL"
+        filenameFmt = "<13sL"
         header = struct.unpack_from(headerFmt, rawData, offset)
         dynTag = "Dynamix Volume File\0"
         if header[0] != dynTag:
@@ -26,24 +26,42 @@ for importFilename in importFilenames:
         offset += struct.calcsize(headerFmt)
         firstFileOffset = offset
         validTypes = ["OVL", "FMD", "DAT"]
+        firstFile = None
         while firstFileIndex == -1:
             firstFileIndex = rawData.find(".", firstFileOffset)
-            firstFile = rawData[firstFileIndex + 1:firstFileIndex + 4]
-            if firstFile not in validTypes:
+            firstFileExt = rawData[firstFileIndex + 1:firstFileIndex + 4]
+            if firstFileExt not in validTypes:
                 firstFileOffset = firstFileIndex + 4
                 firstFileIndex = -1
+                continue
+            endOfFileIndex = firstFileIndex + 4 + 1
+            while rawData[endOfFileIndex] == "\0":
+                endOfFileIndex += 1
+            endOfFileIndex += struct.calcsize("<L")
+            startOfFileIndex = endOfFileIndex - struct.calcsize(filenameFmt)
+            offset = startOfFileIndex
+            firstFile = struct.unpack_from(filenameFmt, rawData, offset)
+            offset += struct.calcsize(filenameFmt)
 
-        firstFile = "SXCODE.OVL"
-        secondFile = "SQ_MARK.DAT"
-        thirdFile = "KEYS.DAT"
-        fourthFile = "SHADES.DAT"
+        if firstFile is None:
+            raise ValueError("Could not find first file to determine first offset")
+        files = []
+        currentFile = firstFile
+        files.append(currentFile)
+        for i in range(98):
+            offset += currentFile[-1]
+            if offset + struct.calcsize(filenameFmt) > len(rawData):
+                break
+            while rawData[offset] == "\0":
+                offset += 1
+            if (offset + struct.calcsize(filenameFmt)) >= len(rawData):
+                break
+            currentFile = struct.unpack_from(filenameFmt, rawData, offset)
+            files.append(currentFile)
+            offset += struct.calcsize(filenameFmt)
 
-
-
-        secondIndex = rawData.index(firstFile)
-        thirdIndex = rawData.index(secondFile)
-        print (secondIndex, thirdIndex, thirdIndex - secondIndex)
-
+        for file in files:
+            print file
 
     except Exception as e:
         print e
