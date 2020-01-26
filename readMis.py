47c1c240813972694c448f31c571767556ec9324
Matthew <matthew@sobantu.co.za>
2020-01-26 19:26:09 +0200

    Mostly working group parsing for MIS files

--- readMis.py
+++ readMis.py
@@ -8,24 +8,45 @@ importFilenames = sys.argv[1:]
 with open(importFilenames[0], "rb") as input_fd:
     rawData = input_fd.read()
 
-skipExtra = {
-    b"ESpt",
+knownTags = {
     b"SIMG",
+    b"SVol",
+    b"ESpt",
+	b"STER",
+	b"SKYT",
+	b"SKPL",
+    b"SKSF",
     b"HERC",
-    b"TANK"
+    b"TANK",
+    b"TRRT",
+	b"FLYR",
+	b"ESNM",
+    b"ESDP",
+    b"SCPG",
+    b"Ssgp",
+    b"\x00\x01\x00\x00",
+    b"\x0c\x01\x00\x00",
+    b"\x00\x00\x00\x0c",
+    b"DPNT",
+    b"trgr",
+	b"mark",
+    b"SNOW"
 }
 
-def readGroupString(rawData, offset, index):
-    (offset, result) = readString(rawData, offset)
+def readGroupString(rawData, offset, index, length):
+    (offset, result) = readString(rawData, offset, index != length - 1)
 
     if (index == 0 and result == ""):
-        (offset, result) = readString(rawData, offset)
+        (offset, result) = readString(rawData, offset, index != length - 1)
     return (offset, result)
 
-def readString(rawData, offset):
+def readString(rawData, offset, seekExtra = True):
     stringStruct = "<B"
     terminatorStruct = "<s"
     (length, ) = struct.unpack_from(stringStruct, rawData, offset)
+    if length > 25:
+        return (offset, "")
+
     offset += struct.calcsize(stringStruct)
     if length == 0:
         return (offset, "")
@@ -35,11 +56,12 @@ def readString(rawData, offset):
 
     offset += struct.calcsize(finalStruct)
 
-    if offset != len(rawData):
+    if offset != len(rawData) and seekExtra is True:
         (terminator, ) = struct.unpack_from(terminatorStruct, rawData, offset)
 
-        if (terminator == b"\0"):
+        while (terminator == b"\0"):
             offset += struct.calcsize(terminatorStruct)
+            (terminator,) = struct.unpack_from(terminatorStruct, rawData, offset)
 
     return (offset, someString)
 
@@ -51,7 +73,7 @@ def readSimVol(objectTypes, rawData, offset):
     header = struct.unpack_from(simVol, rawData, offset)
     offset += struct.calcsize(simVol)
     (offset, volumeString) = readString(rawData, offset)
-    print("SimVol in use is " + volumeString.decode("utf8"))
+    print(b"SimVol in use is " + volumeString)
     return (offset, volumeString)
 
 def readSimGroup(objectTypes, rawData, offset):
@@ -63,21 +85,28 @@ def readSimGroup(objectTypes, rawData, offset):
     i = 0
     while i < header[-1]:
         objectInfo = struct.unpack_from(object, rawData, offset)
-        print("Unpacked a " + objectInfo[0].decode("utf8"))
+        if (objectInfo[0] not in knownTags):
+            currentTag = objectInfo[0]
+            objectInfo = struct.unpack_from(object, rawData, offset + 1)
+            offset += 1
+            if (objectInfo[0] not in knownTags):
+                raise ValueError(b"Could not parse object at index " + str(offset).encode("utf8") + b". Possible tags are " + currentTag + b" or " + objectInfo[0])
+
+
+
+        print(b"Unpacked a " + objectInfo[0])
         children.append(objectInfo)
         if objectInfo[0] in objectTypes:
             (newOffset, moreChildren) = objectTypes[objectInfo[0]](objectTypes, rawData, offset)
             offset = newOffset
         else:
             offset += objectInfo[1] + 8
-            if rawData[offset] == 0 and objectInfo[0] in skipExtra:
-                offset += 1
         i += 1
 
     i = 0
     print("This group has " + str(header[-1]) + " children")
-    while i < header[-1]:
-        (newOffset, someString) = readGroupString(rawData, offset, i)
+    while i < len(children):
+        (newOffset, someString) = readGroupString(rawData, offset, i, len(children))
         offset = newOffset
         if someString == "":
             break
@@ -97,5 +126,5 @@ offset = 0
 try:
     print(readSimGroup(objectTypes, rawData, offset))
 except Exception as e:
-    print(e)
+    print(e, offset)
 
