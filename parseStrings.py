e523400aeeaf7f5d5fe5c0724d936cba1406c2d2
Matthew Rindel <matthew@sobantu.co.za>
2020-02-12 13:52:14 +0200

    Added code to parse a strings constants file

---
+++ parseStrings.py
@@ -0,0 +1,19 @@
+def parseStringsFromFile(importFilename):
+    translations = {}
+    with open(importFilename, "r") as stringFile:
+        lines = stringFile.read().splitlines()
+        for line in lines:
+            if "=" in line and ";" in line:
+                (name, values) = line.split("=")
+                name = name.strip()
+                splitValues = values.split(",")
+                values = ";".join(values.replace(splitValues[0] + ",", "").strip().split(";")[:-1], )
+
+                translations[name] = {
+                    "constantName": name,
+                    "numericTag": splitValues[0].strip(),
+                    "text": {
+                        "en": eval(values)
+                    }
+                }
+    return translations
