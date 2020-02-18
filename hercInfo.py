a5fec5f9ee23c7603f4bce6d21fcb76231229d4c
Matthew <matthew@sobantu.co.za>
2020-02-18 20:00:47 +0200

    Updated code to support parsing weapons and hercs

--- hercInfo.py
+++ hercInfo.py
@@ -1,11 +1,59 @@
+from functools import partial
+import intMountsInfo
 
 def createExecContext():
+    contextToOverwrite = intMountsInfo.createExecContext()
     result = {
-        "hercs": []
-        "currentHerc": {}
+        "hercs": [],
+        "currentHerc": {},
+        "H": "human",
+        "C": "cybrid",
+        "X": "xl",
+        "x": "xl",
+        "S": "small",
+        "L": "large",
+        "M": "medium",
+        "T": "top",
+        "B": "bottom",
+        "LeftPod": "leftPod",
+        "RightPod": "rightPod",
+        "LeftServos": "leftServos",
+        "RightServos": "rightServos",
+        "Pelvis": "pelvis",
+        "none": None,
+        "LeftLeg": "leftLeg",
+        "RightLeg": "rightLeg",
+        "LeftCalf": "leftCalf",
+        "RightCalf": "rightCalf",
+        "LeftFoot": "leftFoot",
+        "RightFoot": "rightFoot",
+        "Pilot": "pilot",
+        "Engine": "engine",
+        "Armor": "armor",
+        "Reactor": "reactor",
+        "Head": "head",
+        "Computer": "computer",
+        "Shield": "shield",
+        "Sensors": "sensor"
     }
+    result["hercBase"] = partial(hercBase, result)
+    result["hercPos"] = partial(hercPos, result)
+    result["hercRot"] = partial(hercRot, result)
+    result["hercAnim"] = partial(hercAnim, result)
+    result["hercCpit"] = partial(hercCpit, result)
+    result["hercColl"] = partial(hercColl, result)
+    result["hercAI"] = partial(hercAI, result)
+    result["newHardPoint"] = partial(newHardPoint, result)
+    result["newMountPoint"] = partial(newMountPoint, result)
+    result["newComponent"] = partial(newComponent, result)
+    result["newConfiguration"] = partial(newConfiguration, result)
+    result["defaultWeapons"] = partial(defaultWeapons, result)
+    result["defaultMountables"] = partial(defaultMountables, result)
 
-    return result
+    for key in result:
+        contextToOverwrite[key] = result[key]
+
+    return contextToOverwrite
 
 def hercBase(context, identityTag, abbreviation, shape, mass, maxMass, radarCrossSection, techLevel, combatValue):
     herc = {
@@ -16,7 +64,13 @@ def hercBase(context, identityTag, abbreviation, shape, mass, maxMass, radarCros
         "maxMass": maxMass,
         "radarCrossSection": radarCrossSection,
         "techLevel": techLevel,
-        "combatValue": combatValue
+        "combatValue": combatValue,
+        "hardPoints": [],
+        "mountPoints": [],
+        "components": [],
+        "configurations": [],
+        "defaultWeapons": [],
+        "defaultMountables": []
     }
     context["currentHerc"] = herc
     context["hercs"].append(herc)
@@ -31,56 +85,86 @@ def hercPos(context, maxPosAcc, minPosVel, maxForPosVel, maxRevPosVel):
 
 def hercRot(context,  minRotVel,    maxRVSlow,        maxRVFast):
     context["currentHerc"]["rot"] = {
-        "maxPosAcc": maxPosAcc,
-        "minPosVel": minPosVel,
-        "maxForPosVel": maxForPosVel,
-        "maxRevPosVel": maxRevPosVel
+        "minRotVel": minRotVel,
+        "maxRVSlow": maxRVSlow,
+        "maxRVFast": maxRVFast
     }
 
 def hercAnim(context, toStandVel, toRunVel, toFastRunVel, toFastTurnVel):
     context["currentHerc"]["anim"] = {
-        "maxPosAcc": maxPosAcc,
-        "minPosVel": minPosVel,
-        "maxForPosVel": maxForPosVel,
-        "maxRevPosVel": maxRevPosVel
+        "toStandVel": toStandVel,
+        "toRunVel": toRunVel,
+        "toFastRunVel": toFastRunVel,
+        "toFastTurnVel": toFastTurnVel
     }
 
 def hercCpit(context, offsetX, offsetY, offsetZ):
     context["currentHerc"]["anim"] = {
-        "maxPosAcc": maxPosAcc,
-        "minPosVel": minPosVel,
-        "maxForPosVel": maxForPosVel,
-        "maxRevPosVel": maxRevPosVel
+        "offsetX": offsetX,
+        "offsetY": offsetY,
+        "offsetZ": offsetZ
     }
 
 def hercColl(context, sphOffstX, sphOffstY, sphOffstZ, sphereRad):
     context["currentHerc"]["coll"] = {
-        "maxPosAcc": maxPosAcc,
-        "minPosVel": minPosVel,
-        "maxForPosVel": maxForPosVel,
-        "maxRevPosVel": maxRevPosVel
+        "sphOffstX": sphOffstX,
+        "sphOffstY": sphOffstY,
+        "sphOffstZ": sphOffstZ,
+        "sphereRad": sphereRad
     }
 
 def hercAI(context, aiName1, aiName2, aiName3, aiName4):
     context["currentHerc"]["ai"] = {
-        "maxPosAcc": maxPosAcc,
-        "minPosVel": minPosVel,
-        "maxForPosVel": maxForPosVel,
-        "maxRevPosVel": maxRevPosVel
+        "aiName1": aiName1,
+        "aiName2": aiName2,
+        "aiName3": aiName3,
+        "aiName4": aiName4
     }
 
-def hercAI(context, aiName1, aiName2, aiName3, aiName4):
-    context["currentHerc"]["ai"] = {
-        "maxPosAcc": maxPosAcc,
-        "minPosVel": minPosVel,
-        "maxForPosVel": maxForPosVel,
-        "maxRevPosVel": maxRevPosVel
-    }
-
-def newHardPoint(context, hardpointId, size, dmgParent, allowedMountables):
-    context["currentHerc"]["hardpoints"].append({
-        "maxPosAcc": maxPosAcc,
-        "minPosVel": minPosVel,
-        "maxForPosVel": maxForPosVel,
-        "maxRevPosVel": maxRevPosVel
+def newHardPoint(context, hardpointId, size, side, dmgParent, offsetFromNodeX, offsetFromNodeY, offsetFromNodeZ, xRotationRangeMin, xRotationRangeMax, zRotationRangeMin, zRotationRangeMax):
+    context["currentHerc"]["hardPoints"].append({
+        "hardpointId": hardpointId,
+        "size": size,
+        "side": side,
+        "dmgParent": dmgParent,
+        "offsetFromNode": [offsetFromNodeX, offsetFromNodeY, offsetFromNodeZ],
+        "xRotationRange": [xRotationRangeMin, xRotationRangeMax],
+        "zRotationRange": [zRotationRangeMin, zRotationRangeMax]
     })
+
+def newMountPoint(context, mountPointId, size, dmgParent, *allowedMountables):
+    mountPoint = {
+        "mountPointId": mountPointId,
+        "size": size,
+        "dmgParent": dmgParent,
+        "allowedMountables": []
+    }
+
+    for mountable in allowedMountables:
+        mountPoint["allowedMountables"].append(mountable)
+    context["currentHerc"]["mountPoints"].append(mountPoint)
+
+
+def newComponent(context, componentId, componentType, parent, maxDamage, identityTag):
+    context["currentHerc"]["components"].append({
+        "componentId": componentId,
+        "componentType": componentType,
+        "parent": parent,
+        "maxDamage": maxDamage,
+        "identityTag": identityTag
+    })
+
+def newConfiguration(context, containee, containter, internalPercentage):
+    context["currentHerc"]["configurations"].append({
+        "containee": containee,
+        "container": containter,
+        "internalPercentage": internalPercentage
+    })
+
+def defaultWeapons(context, *weapons):
+    for weapon in weapons:
+        context["currentHerc"]["defaultWeapons"].append(weapon)
+
+def defaultMountables(context, *mountables):
+    for mountable in mountables:
+        context["currentHerc"]["defaultMountables"].append(mountable)
--- makeJson.py
+++ makeJson.py
@@ -4,8 +4,12 @@ import reactorInfo
 import sensorInfo
 import armorInfo
 import shieldInfo
+import weaponInfo
+import intMountsInfo
 import projectileInfo
+import hercInfo
 import json
+import glob
 
 globalStrings = parseStrings.parseStringsFromFile("Sim.Strings.cs")
 
@@ -14,11 +18,23 @@ def convertToJson(filename, module, finalKey):
 
     with open(f"{filename}.cs", "r") as stringFile:
         lines = stringFile.read().splitlines()
+        skipLine = False
         for line in lines:
+            if skipLine == True:
+                skipLine = False
+                continue
             line = line.strip()
             if line.startswith("//"):
                 continue
-            exec(line, globalStrings, context)
+            try:
+                exec(line, globalStrings, context)
+            except SyntaxError as error:
+                if error.msg.find("EOF") != -1:
+                    nextLine = lines[lines.index(line) + 1]
+                    exec(line + nextLine, globalStrings, context)
+                    skipLine = True
+                else:
+                    print(error)
 
     with open(f"{filename}.json", "w") as outputFile:
         outputFile.write(json.dumps({finalKey: context[finalKey]}, indent="\t"))
@@ -29,4 +45,11 @@ convertToJson("datReactor", reactorInfo, "reactors")
 convertToJson("datSensor", sensorInfo, "sensors")
 convertToJson("datArmor", armorInfo, "armors")
 convertToJson("datShield", shieldInfo, "shields")
+convertToJson("datWeapon", weaponInfo, "weapons")
+convertToJson("datIntMounts", intMountsInfo, "internalMounts")
 convertToJson("datProjectile", projectileInfo, "projectiles")
+
+hercs = glob.glob("datHerc_*.cs")
+
+for herc in hercs:
+    convertToJson(herc.replace(".cs", ""), hercInfo, "hercs")
