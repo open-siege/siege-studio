0c5123cd117fa968f453b61b921008d6b4273fba
Matthew <matthew@sobantu.co.za>
2020-02-23 14:37:58 +0200

    Started first refactor of readVeh.py

--- readVeh.py
+++ readVeh.py
@@ -4,38 +4,6 @@ import json
 
 importFilenames = []
 
-VEHICLE_ID = 26
-ENGINE_ID = 28
-REACTOR_ID = 30
-COMPUTER_ID = 32
-SHIELD_ID = 34
-ARMOR_ID = 36
-
-ENGINE_GROUP_ID_START = 124
-ENGINE_ID_START = 46
-ENGINE_ID_DELTA = 64
-ENGINE_ID_GROUP_MAX_COUNT = 4
-
-REACTOR_GROUP_ID_START = 170
-REACTOR_ID_START = 93
-REACTOR_ID_DELTA = 128
-REACTOR_ID_GROUP_MAX_COUNT = 2
-
-COMPUTER_GROUP_ID_START = 214
-COMPUTER_ID_START = 99
-COMPUTER_ID_DELTA = 128
-COMPUTER_ID_GROUP_MAX_COUNT = 2
-
-SHIELD_GROUP_ID_START = 247
-SHIELD_ID_START = 24
-SHIELD_ID_DELTA = 32
-SHIELD_ID_GROUP_MAX_COUNT = 6
-
-ARMOR_GROUP_ID_START = 101
-ARMOR_ID_START = 86
-ARMOR_ID_DELTA = 32
-ARMOR_ID_GROUP_MAX_COUNT = 6
-
 for importFilename in sys.argv[1:]:
     files = glob.glob(importFilename)
     importFilenames.extend(files)
@@ -44,6 +12,8 @@ def readDataFile(filename):
     with open(filename, "r") as vehicleInfo:
         return json.loads(vehicleInfo.read())
 
+
+constants = readDataFile("vehFileConstants.json")
 vehicleData = readDataFile("datVehicle.json")
 engineData = readDataFile("datEngine.json")
 reactorData = readDataFile("datReactor.json")
@@ -51,29 +21,29 @@ mountData = readDataFile("datIntMounts.json")
 shieldData = readDataFile("datShield.json")
 armorData = readDataFile("datArmor.json")
 
-def generateRawIds(collection, collectionIdName,groupIdStart, componentIdStart, maxCount, delta):
+def generateRawIds(collection, collectionIdName, componentInfo):
     results = []
-    groupId = groupIdStart
-    engineId = componentIdStart
+    groupId = componentInfo["groupIdStart"]
+    engineId = componentInfo["componentIdStart"]
     for item in collection:
         results.append({
             "datId": item[collectionIdName],
             "fileIds": (groupId, engineId),
             "item": item
         })
-        if len(results) % maxCount == 0:
+        if len(results) % componentInfo["groupMaxCount"] == 0:
             groupId += 1
-            engineId = componentIdStart
+            engineId = componentInfo["componentIdStart"]
         else:
-            engineId += delta
+            engineId += componentInfo["idDelta"]
     return results
 
 
-engines = generateRawIds(engineData["engines"], "engineId", ENGINE_GROUP_ID_START, ENGINE_ID_START, ENGINE_ID_GROUP_MAX_COUNT, ENGINE_ID_DELTA)
-reactors = generateRawIds(reactorData["reactors"], "reactorId", REACTOR_GROUP_ID_START, REACTOR_ID_START, REACTOR_ID_GROUP_MAX_COUNT, REACTOR_ID_DELTA)
-computers = generateRawIds(filter(lambda x: x["componentType"] == "computer", mountData["internalMounts"]), "internalMountId", COMPUTER_GROUP_ID_START, COMPUTER_ID_START, COMPUTER_ID_GROUP_MAX_COUNT, COMPUTER_ID_DELTA)
-shields = generateRawIds(shieldData["shields"], "shieldId", SHIELD_GROUP_ID_START, SHIELD_ID_START, SHIELD_ID_GROUP_MAX_COUNT, SHIELD_ID_DELTA)
-armors = generateRawIds(armorData["armors"], "armorId", ARMOR_GROUP_ID_START, ARMOR_ID_START, ARMOR_ID_GROUP_MAX_COUNT, ARMOR_ID_DELTA)
+engines = generateRawIds(engineData["engines"], "engineId", constants["engine"])
+reactors = generateRawIds(reactorData["reactors"], "reactorId", constants["reactor"])
+computers = generateRawIds(filter(lambda x: x["componentType"] == "computer", mountData["internalMounts"]), "internalMountId", constants["computer"])
+shields = generateRawIds(shieldData["shields"], "shieldId", constants["shield"])
+armors = generateRawIds(armorData["armors"], "armorId", constants["armor"])
 
 defaultComponent = {
     "item": {
@@ -88,36 +58,38 @@ defaultComponent = {
 }
 
 
+def findComponent(rawVehicle, componentInfo, collection, fallback):
+    index = componentInfo["idIndex"]
+    return next(
+        filter(lambda x: x["fileIds"] == (rawVehicle[index], rawVehicle[index + 1]), collection),
+        fallback)
+def getComponentName(component):
+    return component["item"]["displayInfo"]["longName"]["text"]["en"]
+
 for importFilename in importFilenames:
     with open(importFilename, "rb") as rawVehicleFile:
         rawVehicle = rawVehicleFile.read()
 
-    rawVehicleId = rawVehicle[VEHICLE_ID]
-    rawEngineId = rawVehicle[ENGINE_ID]
+    engineId = constants["engine"]["idIndex"]
+    reactorId = constants["reactor"]["idIndex"]
+    computerId = constants["computer"]["idIndex"]
+    shieldId = constants["shield"]["idIndex"]
+    armorId = constants["armor"]["idIndex"]
+    rawVehicleId = rawVehicle[constants["vehicle"]["idIndex"]]
+    rawEngineId = rawVehicle[engineId]
     matchingInfo = next(filter(lambda x: x["vehicleId"] == rawVehicleId - 140, vehicleData["vehicles"]), None)
     if matchingInfo is not None:
-        matchingEngine = next(filter(lambda x: x["fileIds"] == (rawVehicle[ENGINE_ID], rawVehicle[ENGINE_ID + 1]), engines), defaultComponent)
-        matchingReactor = next(
-            filter(lambda x: x["fileIds"] == (rawVehicle[REACTOR_ID], rawVehicle[REACTOR_ID + 1]), reactors),
-            defaultComponent)
+        matchingEngine = findComponent(rawVehicle, constants["engine"], engines, defaultComponent)
+        matchingReactor = findComponent(rawVehicle, constants["reactor"], reactors, defaultComponent)
+        matchingComputer = findComponent(rawVehicle, constants["computer"], computers, defaultComponent)
+        matchingShield = findComponent(rawVehicle, constants["shield"], shields, defaultComponent)
+        matchingArmor = findComponent(rawVehicle, constants["armor"], armors, defaultComponent)
 
-        matchingComputer = next(
-            filter(lambda x: x["fileIds"] == (rawVehicle[COMPUTER_ID], rawVehicle[COMPUTER_ID + 1]), computers),
-            defaultComponent)
-
-        matchingShield = next(
-            filter(lambda x: x["fileIds"] == (rawVehicle[SHIELD_ID], rawVehicle[SHIELD_ID + 1]), shields),
-            defaultComponent)
-
-        matchingArmor = next(
-            filter(lambda x: x["fileIds"] == (rawVehicle[ARMOR_ID], rawVehicle[ARMOR_ID + 1]), armors),
-            defaultComponent)
-
-        engineName = matchingEngine["item"]["displayInfo"]["longName"]["text"]["en"]
-        reactorName = matchingReactor["item"]["displayInfo"]["longName"]["text"]["en"]
-        computerName = matchingComputer["item"]["displayInfo"]["longName"]["text"]["en"]
-        shieldName = matchingShield["item"]["displayInfo"]["longName"]["text"]["en"]
-        armorName = matchingArmor["item"]["displayInfo"]["longName"]["text"]["en"]
+        engineName = getComponentName(matchingEngine)
+        reactorName = getComponentName(matchingReactor)
+        computerName = getComponentName(matchingComputer)
+        shieldName = getComponentName(matchingShield)
+        armorName = getComponentName(matchingArmor)
 
         print(importFilename, ": ", ", ".join([matchingInfo["identityTag"]["text"]["en"],engineName, reactorName, computerName, shieldName, armorName]))
     else:
