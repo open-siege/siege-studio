518ead92aff320565c61dce613591e07639adcd9
Matthew Rindel <matthew@sobantu.co.za>
2020-02-28 15:32:05 +0200

    Added support for showing progress of download and better version checking

--- sspm.py
+++ sspm.py
@@ -1,9 +1,8 @@
 import json
 import tarfile
 import requests
+from clint.textui import progress
 import os.path
-from io import BytesIO
-
 
 packageName = "starsiege-sounds"
 with open("sspm.config.json", "r") as configFile:
@@ -19,34 +18,54 @@ def getPackageInfo(packageName):
     return packageInfoResponse.json()
 
 def downloadTarball(versionInfo):
-    packageInfoResponse = requests.get(versionInfo['dist']['tarball'])
+    packageInfoResponse = requests.get(versionInfo['dist']['tarball'], stream=True)
     return packageInfoResponse
 
 def getTarballName(versionInfo):
     return os.path.split(versionInfo['dist']['tarball'])[-1]
 
+def findMatchingVersion(packageInfo, version):
+    version = version.split("^")[-1]
+    for someVersion in packageInfo["versions"]:
+        if someVersion == version:
+            return someVersion
+    return None
+
 def downloadPackageWithDependencies(packageName, version = None):
-    print(f"downloading {packageName}")
     destinationDirectory = os.path.join(tempDirectory, packageName)
     if not os.path.exists(destinationDirectory):
         os.makedirs(destinationDirectory)
     packageInfo = getPackageInfo(packageName)
+
     if version is None:
-        version = packageInfo["dist-tags"]["latest"]
+        versionToUse = packageInfo["dist-tags"]["latest"]
+    else:
+        versionToUse = findMatchingVersion(packageInfo, version)
 
-    versionInfo = packageInfo["versions"][version]
+    if versionToUse is None:
+        raise RuntimeError(f"Could not find matching version for {packageName} {version}")
 
-    tarballResponse = downloadTarball(versionInfo)
+    print(f"downloading {packageName} {versionToUse}")
+    versionInfo = packageInfo["versions"][versionToUse]
 
     with open(getTarballName(versionInfo), "wb") as tempFile:
-        tempFile.write(tarballResponse.content)
+        tarballResponse = downloadTarball(versionInfo)
+        tarLength = int(tarballResponse.headers.get("content-length"))
+
+        if tarLength is None:
+            tempFile.write(tarballResponse.content)
+        else:
+            for chunk in progress.bar(tarballResponse.iter_content(chunk_size=1024), expected_size=(tarLength / 1024) + 1):
+                if chunk:
+                    tempFile.write(chunk)
+                    tempFile.flush()
 
     with tarfile.open(getTarballName(versionInfo)) as tf:
         tf.extractall(path=destinationDirectory)
 
     if "dependencies" in versionInfo:
-        for key in versionInfo["dependencies"]:
-            downloadPackageWithDependencies(key)
+        for key, value in versionInfo["dependencies"].items():
+            downloadPackageWithDependencies(key, value)
 
 
 downloadPackageWithDependencies(packageName)
