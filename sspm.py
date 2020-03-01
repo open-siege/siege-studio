f276dbb437d91d9e9e855d954e8deb6b5dc8b859
Matthew <matthew@sobantu.co.za>
2020-03-01 12:23:07 +0200

    First mostly working version of sspm

--- sspm.py
+++ sspm.py
@@ -1,17 +1,32 @@
 import json
 import tarfile
 import requests
-from clint.textui import progress
+import os
 import os.path
+from clint.textui import progress
+from os import walk
 
-packageName = "starsiege-sounds"
+packageName = "starsiege-retail"
 with open("sspm.config.json", "r") as configFile:
     config = json.loads(configFile.read())
 
-tempDirectory = "temp/packages"
+tempDirectory = os.path.join("temp", "packages")
+finalDirectory = "final"
 
 if not os.path.exists(tempDirectory):
     os.makedirs(tempDirectory)
+
+def getAllFilesFromFolder(folder, files = None):
+    if files is None:
+        files = []
+
+    for (dirpath, dirnames, filenames) in walk(folder):
+        filenames = map(lambda f: os.path.join(dirpath, f), filenames)
+        files.extend(filenames)
+        for dir in dirnames:
+            getAllFilesFromFolder(dir, files)
+
+    return files
 
 def getPackageInfo(packageName):
     packageInfoResponse = requests.get(f"{config['packageRegistry']}/{packageName}")
@@ -31,6 +46,23 @@ def findMatchingVersion(packageInfo, version):
             return someVersion
     return None
 
+def copyFilesToFinalFolder(packageInfo):
+    for file in packageInfo["extractedFiles"]:
+
+        print(file)
+        fileFolder = file.replace(os.path.join(tempDirectory, packageInfo["name"], "package", ""), "")
+        splitData = os.path.split(fileFolder)
+        fileFolder = os.path.join(finalDirectory, *splitData[:-1])
+        filename = splitData[-1]
+        if not os.path.exists(fileFolder):
+            os.makedirs(fileFolder)
+        if filename != "package.json":
+            if not os.path.exists(os.path.join(fileFolder, filename)):
+                os.rename(file, os.path.join(fileFolder, filename))
+    for child in packageInfo["expandedDependencies"]:
+        copyFilesToFinalFolder(child)
+
+
 def downloadPackageWithDependencies(packageName, version = None):
     destinationDirectory = os.path.join(tempDirectory, packageName)
     if not os.path.exists(destinationDirectory):
@@ -48,25 +80,33 @@ def downloadPackageWithDependencies(packageName, version = None):
     print(f"downloading {packageName} {versionToUse}")
     versionInfo = packageInfo["versions"][versionToUse]
 
-    with open(getTarballName(versionInfo), "wb") as tempFile:
-        tarballResponse = downloadTarball(versionInfo)
-        tarLength = int(tarballResponse.headers.get("content-length"))
+    if not os.path.exists(os.path.join(tempDirectory, getTarballName(versionInfo))):
+        with open(os.path.join(tempDirectory, getTarballName(versionInfo)), "wb") as tempFile:
+            tarballResponse = downloadTarball(versionInfo)
+            tarLength = int(tarballResponse.headers.get("content-length"))
 
-        if tarLength is None:
-            tempFile.write(tarballResponse.content)
-        else:
-            for chunk in progress.bar(tarballResponse.iter_content(chunk_size=1024), expected_size=(tarLength / 1024) + 1):
-                if chunk:
-                    tempFile.write(chunk)
-                    tempFile.flush()
+            if tarLength is None:
+                tempFile.write(tarballResponse.content)
+            else:
+                for chunk in progress.bar(tarballResponse.iter_content(chunk_size=1024), expected_size=(tarLength / 1024) + 1):
+                    if chunk:
+                        tempFile.write(chunk)
+                        tempFile.flush()
 
-    with tarfile.open(getTarballName(versionInfo)) as tf:
+
+    with tarfile.open(os.path.join(tempDirectory, getTarballName(versionInfo))) as tf:
         tf.extractall(path=destinationDirectory)
 
+    print(destinationDirectory)
+    packageInfo["extractedFiles"] = getAllFilesFromFolder(destinationDirectory)
+
+    packageInfo["expandedDependencies"] = []
     if "dependencies" in versionInfo:
         for key, value in versionInfo["dependencies"].items():
-            downloadPackageWithDependencies(key, value)
+            childPackage = downloadPackageWithDependencies(key, value)
+            packageInfo["expandedDependencies"].append(childPackage)
 
+    return packageInfo
 
-downloadPackageWithDependencies(packageName)
-
+finalPackage = downloadPackageWithDependencies(packageName)
+copyFilesToFinalFolder(finalPackage)
