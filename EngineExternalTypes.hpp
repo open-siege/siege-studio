b88d3773f057532c884015017a8eca54a1d4f811
Matthew <matthew@sobantu.co.za>
2020-06-13 06:21:43 +0200

    Updated external types to use string_view

--- EngineExternalTypes.hpp
+++ EngineExternalTypes.hpp
@@ -5,24 +5,25 @@
 
 #include <vector>
 #include <string>
+#include <string_view>
 
 namespace Engine
 {
 	struct ExternalConsoleCallback
 	{
-		virtual std::string doExecuteCallback(void* console, std::int32_t callbackId, std::vector<std::string>& args) = 0;
+		virtual std::string doExecuteCallback(void* console, std::int32_t callbackId, const std::vector<std::string_view>& args) = 0;
 		virtual ~ExternalConsoleCallback() = default;
 	};
 
 	struct ExternalConsoleConsumer
 	{
-		virtual void doWriteLine(void* console, const std::string& line) = 0;
+		virtual void doWriteLine(void* console, const std::string_view& line) = 0;
 		virtual ~ExternalConsoleConsumer() = default;
 	};
 
 	struct ExternalGamePlugin
 	{
-		virtual std::string doExecuteCallback(void* console, std::int32_t callbackId, std::vector<std::string>& args) = 0;
+		virtual std::string doExecuteCallback(void* console, std::int32_t callbackId, const std::vector<std::string_view>& args) = 0;
 
 		virtual void doInit() = 0;
 		virtual void doStartFrame() = 0;
@@ -49,7 +50,7 @@ namespace Engine
 				{
 					try
 					{
-						std::vector<std::string> arguments(argv, argv + argc);
+						std::vector<std::string_view> arguments(argv, argv + argc);
 						_lastResult = _internalCallback->doExecuteCallback(console, callbackId, arguments);
 
 					}
