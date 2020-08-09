f4ac00da074bd6d5a58ae41d061efb9448eda42a
Matthew <matthew@sobantu.co.za>
2020-08-09 11:53:27 +0200

    Refactored structures to have sanely grouped namespaces Moved find_files into a shared header Fixed bug with not finding the right files Added TODOs for changes to be made

--- src/json_boost.hpp
+++ src/json_boost.hpp
@@ -12,14 +12,18 @@ namespace nlohmann
   struct adl_serializer<std::array<char, Size>>
   {
     template<typename BasicJsonType>
-    static void to_json(BasicJsonType &j, const std::array<char, Size> &opt)
+    static void to_json(BasicJsonType& j, const std::array<char, Size>& opt)
     {
       j = std::string(&opt[0]);
     }
 
     template<typename BasicJsonType>
-    static void from_json(const BasicJsonType &j, std::array<char, Size> &opt)
+    static void from_json(const BasicJsonType& j, std::array<char, Size>& opt)
     {
+      /* TODO: throw exception if the input string size is too big for the array.
+       * You actually wouldn't want things to silently fail because a bad DTS/DML file will crash
+       * the respective game.
+       * */
       auto result = j.template get<std::string>();
       auto end = result.size() < Size ? result.end() : result.begin() + Size - 1;
 
@@ -34,13 +38,13 @@ namespace nlohmann
     using EndianType = boost::endian::endian_arithmetic<ByteOrder, IntType, Size>;
 
     template<typename BasicJsonType>
-    static void to_json(BasicJsonType &j, const EndianType &opt)
+    static void to_json(BasicJsonType& j, const EndianType& opt)
     {
       j = static_cast<typename EndianType::value_type>(opt);
     }
 
     template<typename BasicJsonType>
-    static void from_json(const BasicJsonType &j, EndianType &opt)
+    static void from_json(const BasicJsonType& j, EndianType& opt)
     {
       opt = j.template get<EndianType::value_type>();
     }
@@ -60,7 +64,7 @@ namespace darkstar::dts
   };
 
   template<typename BasicJsonType, std::size_t Size, typename... Args>
-  void from_json_impl(const BasicJsonType &json, std::array<std::string_view, Size> keys, Args &... args)
+  void from_json_impl(const BasicJsonType& json, std::array<std::string_view, Size> keys, Args&... args)
   {
     using key_type = typename BasicJsonType::object_t::key_type;
     std::size_t current_key = 0;
@@ -68,131 +72,131 @@ namespace darkstar::dts
   }
 
   template<typename BasicJsonType, typename StructType, typename = typename std::enable_if_t<has_struct_keys<StructType>::value>>
-  void from_json(const BasicJsonType &json, StructType &raw)
+  void from_json(const BasicJsonType& json, StructType& raw)
   {
     if constexpr (StructType::keys.size() == 1)
     {
-      auto &keys = StructType::keys;
-      auto &[item0] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0] = raw;
 
       from_json_impl(json, keys, item0);
     }
 
     if constexpr (StructType::keys.size() == 2)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1] = raw;
 
       from_json_impl(json, keys, item0, item1);
     }
 
     if constexpr (StructType::keys.size() == 3)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2] = raw;
 
       from_json_impl(json, keys, item0, item1, item2);
     }
 
     if constexpr (StructType::keys.size() == 4)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3] = raw;
 
       from_json_impl(json, keys, item0, item1, item2, item3);
     }
 
     if constexpr (StructType::keys.size() == 5)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4] = raw;
 
       from_json_impl(json, keys, item0, item1, item2, item3, item4);
     }
 
     if constexpr (StructType::keys.size() == 6)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5] = raw;
 
       from_json_impl(json, keys, item0, item1, item2, item3, item4, item5);
     }
 
     if constexpr (StructType::keys.size() == 7)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6] = raw;
 
       from_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6);
     }
 
     if constexpr (StructType::keys.size() == 8)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7] = raw;
 
       from_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7);
     }
 
     if constexpr (StructType::keys.size() == 9)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8] = raw;
 
       from_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8);
     }
 
     if constexpr (StructType::keys.size() == 10)
     {
-      const auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8, item9] = raw;
+      const auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9] = raw;
 
       from_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9);
     }
 
     if constexpr (StructType::keys.size() == 11)
     {
-      const auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10] = raw;
+      const auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10] = raw;
 
       from_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10);
     }
 
     if constexpr (StructType::keys.size() == 12)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11] = raw;
 
       from_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11);
     }
 
     if constexpr (StructType::keys.size() == 13)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12] = raw;
 
       from_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12);
     }
 
     if constexpr (StructType::keys.size() == 14)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13] = raw;
 
       from_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13);
     }
 
     if constexpr (StructType::keys.size() == 15)
     {
-      const auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14] = raw;
+      const auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14] = raw;
 
       from_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14);
     }
   }
 
   template<typename BasicJsonType, std::size_t Size, typename... Args>
-  void to_json_impl(BasicJsonType &json, std::array<std::string_view, Size> keys, Args &... args)
+  void to_json_impl(BasicJsonType& json, std::array<std::string_view, Size> keys, Args&... args)
   {
     using key_type = typename BasicJsonType::object_t::key_type;
     std::size_t current_key = 0;
@@ -201,110 +205,110 @@ namespace darkstar::dts
 
 
   template<typename BasicJsonType, typename StructType, typename = typename std::enable_if_t<has_struct_keys<StructType>::value>>
-  void to_json(BasicJsonType &json, const StructType &raw)
+  void to_json(BasicJsonType& json, const StructType& raw)
   {
     if constexpr (StructType::keys.size() == 1)
     {
-      auto &keys = StructType::keys;
-      auto &[item0] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0] = raw;
       to_json_impl(json, keys, item0);
     }
 
     if constexpr (StructType::keys.size() == 2)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1] = raw;
       to_json_impl(json, keys, item0, item1);
     }
 
     if constexpr (StructType::keys.size() == 3)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2] = raw;
       to_json_impl(json, keys, item0, item1, item2);
     }
 
     if constexpr (StructType::keys.size() == 4)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3] = raw;
       to_json_impl(json, keys, item0, item1, item2, item3);
     }
 
     if constexpr (StructType::keys.size() == 5)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4] = raw;
       to_json_impl(json, keys, item0, item1, item2, item3, item4);
     }
 
     if constexpr (StructType::keys.size() == 6)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5] = raw;
       to_json_impl(json, keys, item0, item1, item2, item3, item4, item5);
     }
 
     if constexpr (StructType::keys.size() == 7)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6] = raw;
       to_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6);
     }
 
     if constexpr (StructType::keys.size() == 8)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7] = raw;
       to_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7);
     }
 
     if constexpr (StructType::keys.size() == 9)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8] = raw;
       to_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8);
     }
 
     if constexpr (StructType::keys.size() == 10)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8, item9] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9] = raw;
       to_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9);
     }
 
     if constexpr (StructType::keys.size() == 11)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10] = raw;
       to_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10);
     }
 
     if constexpr (StructType::keys.size() == 12)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11] = raw;
       to_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11);
     }
 
     if constexpr (StructType::keys.size() == 13)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12] = raw;
       to_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12);
     }
 
     if constexpr (StructType::keys.size() == 14)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13] = raw;
       to_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13);
     }
 
     if constexpr (StructType::keys.size() == 15)
     {
-      auto &keys = StructType::keys;
-      auto &[item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14] = raw;
+      auto& keys = StructType::keys;
+      auto& [item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14] = raw;
       to_json_impl(json, keys, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14);
     }
   }
@@ -315,6 +319,11 @@ namespace darkstar::dts
     using darkstar::dts::from_json;
   }// namespace shape::v2
 
+  namespace shape::v3
+  {
+    using darkstar::dts::to_json;
+    using darkstar::dts::from_json;
+  }// namespace shape::v3
 
   namespace shape::v5
   {
