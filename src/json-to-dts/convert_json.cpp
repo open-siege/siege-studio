6235010e0424f9eec74109dea3062979baa0c600
Matthew <matthew@sobantu.co.za>
2020-08-10 10:17:43 +0200

    Added release flags per compiler Added support for converting json to dml

--- src/json-to-dts/convert_json.cpp
+++ src/json-to-dts/convert_json.cpp
@@ -81,7 +81,10 @@ int main(int argc, const char** argv)
 {
   const auto files = dts::shared::find_files(
     std::vector<std::string>(argv + 1, argv + argc),
-    ".dts.json", ".DTS.json"/*, TODO properly recreate dml files from JSON: ".dml.json", ".DML.json"*/);
+    ".dts.json",
+    ".DTS.json" ,
+    ".dml.json",
+    ".DML.json");
 
   std::for_each(std::execution::par_unseq, files.begin(), files.end(), [](auto&& file_name) {
     try
@@ -94,69 +97,89 @@ int main(int argc, const char** argv)
 
       std::ifstream test_file(file_name);
       auto fresh_shape_json = nlohmann::json::parse(test_file);
-      const dts::shape_variant fresh_shape = fresh_shape_json;
+      const auto json_type_name = fresh_shape_json.at("typeName").get<std::string>();
 
-      auto new_file_name = file_name.string() + ".dts";
+      if (json_type_name == dts::material_list::v2::material_list::type_name)
+      {
+        const dts::material_list_variant fresh_shape = fresh_shape_json;
+        auto new_file_name = file_name.string() + ".dml";
 
-      std::basic_ofstream<std::byte> stream(new_file_name, std::ios::binary);
+        std::basic_ofstream<std::byte> stream(new_file_name, std::ios::binary);
 
-      std::visit([&](const auto& shape) {
-        write_header(stream, shape);
-        write(stream, shape.header);
-        write(stream, shape.data);
-        write(stream, shape.nodes);
-        write(stream, shape.sequences);
-        write(stream, shape.sub_sequences);
-        write(stream, shape.keyframes);
-        write(stream, shape.transforms);
-        write(stream, shape.names);
-        write(stream, shape.objects);
-        write(stream, shape.details);
-        write(stream, shape.transitions);
+        std::visit([&](const auto& materials) {
+                          write_header(stream, materials);
+                          write(stream, materials.header);
+                          write(stream, materials.materials);
+                          write_size(stream);
+                   },
+                   fresh_shape);
+      }
+      else if (json_type_name == dts::shape::v2::shape::type_name)
+      {
+        const dts::shape_variant fresh_shape = fresh_shape_json;
 
-        if constexpr (std::remove_reference_t<decltype(shape)>::version > 3)
-        {
-          write(stream, shape.frame_triggers);
-          write(stream, shape.footer);
-        }
+        auto new_file_name = file_name.string() + ".dts";
 
-        for (const auto& mesh_var : shape.meshes)
-        {
+        std::basic_ofstream<std::byte> stream(new_file_name, std::ios::binary);
+
+        std::visit([&](const auto& shape) {
+          write_header(stream, shape);
+          write(stream, shape.header);
+          write(stream, shape.data);
+          write(stream, shape.nodes);
+          write(stream, shape.sequences);
+          write(stream, shape.sub_sequences);
+          write(stream, shape.keyframes);
+          write(stream, shape.transforms);
+          write(stream, shape.names);
+          write(stream, shape.objects);
+          write(stream, shape.details);
+          write(stream, shape.transitions);
+
+          if constexpr (std::remove_reference_t<decltype(shape)>::version > 3)
+          {
+            write(stream, shape.frame_triggers);
+            write(stream, shape.footer);
+          }
+
+          for (const auto& mesh_var : shape.meshes)
+          {
+            const auto start_offset = static_cast<std::uint32_t>(stream.tellp());
+
+            std::visit([&](const auto& mesh) {
+              write_header(stream, mesh);
+              write(stream, mesh.header);
+              write(stream, mesh.vertices);
+              write(stream, mesh.texture_vertices);
+              write(stream, mesh.faces);
+              write(stream, mesh.frames);
+              write_size(stream, start_offset);
+            },
+              mesh_var);
+          }
+
+          const boost::endian::little_uint32_t has_materials = 1u;
+          write(stream, has_materials);
+
           const auto start_offset = static_cast<std::uint32_t>(stream.tellp());
-
-          std::visit([&](const auto& mesh) {
-            write_header(stream, mesh);
-            write(stream, mesh.header);
-            write(stream, mesh.vertices);
-            write(stream, mesh.texture_vertices);
-            write(stream, mesh.faces);
-            write(stream, mesh.frames);
+          std::visit([&](const auto& materials) {
+            write_header(stream, materials);
+            write(stream, materials.header);
+            write(stream, materials.materials);
             write_size(stream, start_offset);
           },
-            mesh_var);
-        }
+            shape.material_list);
 
-        const boost::endian::little_uint32_t has_materials = 1u;
-        write(stream, has_materials);
-
-        const auto start_offset = static_cast<std::uint32_t>(stream.tellp());
-        std::visit([&](const auto& materials) {
-          write_header(stream, materials);
-          write(stream, materials.header);
-          write(stream, materials.materials);
-          write_size(stream, start_offset);
+          write_size(stream);
         },
-          shape.material_list);
-
-        write_size(stream);
-      },
-        fresh_shape);
+          fresh_shape);
+      }
     }
     catch (const std::exception& ex)
     {
-        std::stringstream msg;
-        msg << file_name << " " << ex.what() << '\n';
-        std::cerr << msg.str();
+      std::stringstream msg;
+      msg << file_name << " " << ex.what() << '\n';
+      std::cerr << msg.str();
     }
   });
 
