e87ad56e10a35fda5b597de11b3e41ffdf99ac46
Matthew <matthew@sobantu.co.za>
2020-09-26 14:54:30 +0200

    Added glm to the project for future animation work Added code to convert short quarts to float quarts Updated default rotation values

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class LocalConanFile(ConanFile):
     system_requires = "opengl/system"
     build_requires = "cmake/3.17.3", "cppcheck_installer/2.0@bincrafters/stable"
     settings = "os", "compiler", "build_type", "arch"
-    requires = "toml11/3.4.0", "nlohmann_json/3.9.0", "boost_endian/1.69.0@bincrafters/stable", "imgui-sfml/2.1@bincrafters/stable", "wxwidgets/3.1.3@bincrafters/stable"
+    requires = "toml11/3.4.0", "nlohmann_json/3.9.0", "boost_endian/1.69.0@bincrafters/stable", "imgui-sfml/2.1@bincrafters/stable", "wxwidgets/3.1.3@bincrafters/stable", "glm/0.9.9.8"
     generators = "cmake", "virtualenv"
     build_folder = "build"
 
