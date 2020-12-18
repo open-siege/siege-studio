9cb0c19c17608d58da6d22464533eeb2b6619bb8
Matthew <matthew@sobantu.co.za>
2020-12-18 15:17:00 +0200

    Created implementation file for the volume code and moved relevant logic there

--- src/archives/darkstar_volume.hpp
+++ src/archives/darkstar_volume.hpp
@@ -1,11 +1,10 @@
 #ifndef DARKSTARDTSCONVERTER_DARKSTAR_VOLUME_HPP
 #define DARKSTARDTSCONVERTER_DARKSTAR_VOLUME_HPP
 
-#include <fstream>
-#include <vector>
 #include <array>
-#include <utility>
-#include <string>
+#include <vector>
+#include <fstream>
+
 #include "endian_arithmetic.hpp"
 
 namespace darkstar::vol
@@ -88,94 +87,14 @@ namespace darkstar::vol
 
   static_assert(sizeof(file_header) == sizeof(std::array<std::byte, 17>));
 
+  std::size_t get_file_list_offsets(std::basic_ifstream<std::byte>& raw_data);
 
-  std::size_t get_file_list_offsets(std::basic_ifstream<std::byte>& raw_data)
-  {
-    volume_header header;
+  std::vector<std::string> get_file_names(std::basic_ifstream<std::byte>& raw_data);
 
-    raw_data.read(reinterpret_cast<std::byte*>(&header), sizeof(header));
+  std::vector<file_info> get_file_metadata(std::basic_ifstream<std::byte>& raw_data);
 
-    if (header.file_tag == vol_file_tag)
-    {
-      normal_footer footer;
-      raw_data.seekg(header.footer_offset, std::ios_base::beg);
-      raw_data.read(reinterpret_cast<std::byte*>(&footer), sizeof(footer));
-
-      return footer.file_list_size;
-    }
-    else if (header.file_tag == alt_vol_file_tag)
-    {
-      alternative_footer footer;
-      raw_data.seekg(header.footer_offset, std::ios_base::beg);
-      raw_data.read(reinterpret_cast<std::byte*>(&footer), sizeof(footer));
-
-      return footer.file_list_size;
-    }
-    else
-    {
-      throw std::invalid_argument("The file provided is not a valid Darkstar VOL file.");
-    }
-  }
-
-  std::vector<std::string> get_file_names(std::basic_ifstream<std::byte>& raw_data)
-  {
-    auto buffer_size = get_file_list_offsets(raw_data);
-    std::vector<char> raw_chars(buffer_size);
-
-    if ((buffer_size) % 2 != 0)
-    {
-      raw_data.seekg(1, std::ios::cur);
-    }
-
-    raw_data.read(reinterpret_cast<std::byte*>(raw_chars.data()), raw_chars.size());
-
-    std::vector<std::string> results;
-
-    std::size_t index = 0;
-
-    while (index < raw_chars.size())
-    {
-      results.emplace_back(raw_chars.data() + index);
-
-      index += results.back().size() + 1;
-    }
-
-    return results;
-  }
-
-  std::vector<file_info> get_file_metadata(std::basic_ifstream<std::byte>& raw_data)
-  {
-    auto filenames = get_file_names(raw_data);
-    file_index_header header;
-    raw_data.read(reinterpret_cast<std::byte*>(&header), sizeof(header));
-
-    std::vector<std::byte> raw_bytes(header.index_size);
-    raw_data.read(raw_bytes.data(), raw_bytes.size());
-
-    std::size_t index = 0;
-
-    std::vector<file_info> results;
-    results.reserve(filenames.size());
-
-    while (index < raw_bytes.size())
-    {
-      file_header file;
-      std::copy(raw_bytes.data() + index, raw_bytes.data() + index + sizeof(file_header), reinterpret_cast<std::byte*>(&file));
-
-      file_info info;
-
-      info.filename = std::move(filenames[results.size()]);
-      info.size = file.size;
-      info.offset = file.offset;
-      info.compression_type = file.compression_type;
-      results.emplace_back(info);
-      index += sizeof(file_header);
-    }
-
-    return results;
-  }
-}// namespace darkstar
+  void extract_files(std::basic_ifstream<std::byte>& volume, std::string_view volume_filename, std::string_view output_dir, file_info& some_file);
+}// namespace darkstar::vol
 
 
 #endif//DARKSTARDTSCONVERTER_DARKSTAR_VOLUME_HPP
-
