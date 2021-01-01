404da04d886031ca7bd5f0d273aa4b6ef45fa08b
Matthew <matthew@sobantu.co.za>
2021-01-01 11:50:07 +0200

    Updated view factory to make use of extensions Updated main app to make use of extensions container

--- src/3space-studio/views/view_factory.hpp
+++ src/3space-studio/views/view_factory.hpp
@@ -15,7 +15,33 @@ class view_factory
 public:
   void add_file_type(stream_validator* checker, view_creator* creator)
   {
-    creators.emplace_back(checker, creator);
+    creators.emplace(checker, creator);
+  }
+
+  void add_extension(std::string_view extension, stream_validator* checker)
+  {
+    extensions.emplace_back(extension);
+    validators.emplace(extensions.back(), checker);
+  }
+
+  std::vector<std::string_view> get_extensions() const
+  {
+    return std::vector<std::string_view>(extensions.cbegin(), extensions.cend());
+  }
+
+  graphics_view* create_view(std::string_view extension, std::basic_istream<std::byte>& stream) const
+  {
+    auto archive_type = validators.equal_range(extension);
+
+    for (auto it = archive_type.first; it != archive_type.second; ++it)
+    {
+      if (it->second(stream))
+      {
+        return creators.at(it->second)(stream);
+      }
+    }
+
+    return create_view(stream);
   }
 
   graphics_view* create_view(std::basic_istream<std::byte>& stream) const
@@ -32,7 +58,10 @@ public:
   }
 
 private:
-  std::vector<std::pair<stream_validator*, view_creator*>> creators;
+  std::vector<std::string> extensions;
+
+  std::map<stream_validator*, view_creator*> creators;
+  std::multimap<std::string_view, stream_validator*> validators;
 };
 
 view_factory create_default_view_factory();
