139b2df0ef58d730bea817949042052dcead6ba1
Matthew <matthew@sobantu.co.za>
2021-01-04 18:20:03 +0200

    Fixed issue with compression text in vol view Added logo to default view, and wiki link support for unsupported files More files ignored Minor tweaks based on clang tidy feedback

--- src/3space-studio/views/default_view.hpp
+++ src/3space-studio/views/default_view.hpp
@@ -2,14 +2,19 @@
 #define DARKSTARDTSCONVERTER_DEFAULT_VIEW_HPP
 
 #include "graphics_view.hpp"
+#include "archives/file_system_archive.hpp"
 
 class default_view : public graphics_view
 {
-  bool requires_gl() const override { return true; }
-  std::map<sf::Keyboard::Key, std::reference_wrapper<std::function<void(const sf::Event&)>>> get_callbacks() { return {};}
-  void setup_view(wxWindow* parent, sf::RenderWindow* window, ImGuiContext* guiContext) {}
-  void render_gl(wxWindow* parent, sf::RenderWindow* window, ImGuiContext* guiContext) {}
-  void render_ui(wxWindow* parent, sf::RenderWindow* window, ImGuiContext* guiContext) {}
+public:
+  default_view(shared::archive::file_info info);
+  bool requires_gl() const override { return false; }
+  std::map<sf::Keyboard::Key, std::reference_wrapper<std::function<void(const sf::Event&)>>> get_callbacks() override { return {};}
+  void setup_view(wxWindow* parent, sf::RenderWindow* window, ImGuiContext* guiContext) override;
+  void render_gl(wxWindow* parent, sf::RenderWindow* window, ImGuiContext* guiContext) override {}
+  void render_ui(wxWindow* parent, sf::RenderWindow* window, ImGuiContext* guiContext) override {}
+private:
+  shared::archive::file_info info;
 };
 
 #endif//DARKSTARDTSCONVERTER_DEFAULT_VIEW_HPP
