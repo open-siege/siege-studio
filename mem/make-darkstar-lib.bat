ea4de8bf328bb14fd4b13c10f68490af3b6ec8c8
Matthew <matthew@sobantu.co.za>
2021-01-16 10:52:25 +0200

    Revised how project is compiled and built Main conan file makes sure everything is setup and installed Project now structured with each module having its own top level directory

---
+++ mem/make-darkstar-lib.bat
@@ -0,0 +1,2 @@
+lib /def:..\darkstar\src\darkstar.def /out:libs\darkstar.lib /machine:x86
+lib /def:..\darkstar.detours\src\darkstar.detours.def /out:libs\darkstar.detours.lib /machine:x86
--- src/mem/make-darkstar-lib.bat
+++
@@ -1,2 +0,0 @@
-lib /def:..\darkstar\darkstar.def /out:libs\darkstar.lib /machine:x86
-lib /def:..\darkstar.detours\darkstar.detours.def /out:libs\darkstar.detours.lib /machine:x86
