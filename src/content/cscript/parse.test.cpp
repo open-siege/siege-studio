8ebe760d306db2c79923abe63bfc16b9406c27ba
Matthew <matthew@sobantu.co.za>
2021-02-12 17:49:26 +0200

    Added first instance of pegtl for future parsing of script files

---
+++ src/content/cscript/parse.test.cpp
@@ -0,0 +1,48 @@
+#include <catch2/catch.hpp>
+#include <string>
+#include <iostream>
+#include <functional>
+#include <tao/pegtl/contrib/parse_tree.hpp>
+#include "lua53.hpp"
+
+TEST_CASE("Text is parsed correctly", "[pal.microsoft]")
+{
+  auto content = R"(function onAbilityCast(caster, targetPos)
+    local units = findUnitsInRadius(targetPos, 500)
+    local enemies = __TS__ArrayFilter(
+        units,
+        function(____, unit) return caster:isEnemy(unit) end
+    )
+    for ____, enemy in ipairs(enemies) do
+        enemy:kill()
+    end
+end)";
+
+
+  tao::pegtl::string_input in(content, "from_content");
+  auto root = tao::pegtl::parse_tree::parse<lua53::grammar>(in);
+
+  int level = 0;
+  std::function<void(const tao::pegtl::parse_tree::node&)> write_node = [&](auto& child) {
+    if (child.has_content())
+    {
+      std::cout << "Level " << level << " " << child.type << " " << child.string_view() << std::endl;
+    }
+    else
+    {
+      std::cout << "Level " << level << " " << child.type << std::endl;
+    }
+
+
+    level++;
+    for (auto& child_child : child.children)
+    {
+      write_node(*child_child);
+    }
+    level--;
+  };
+
+  write_node(*root);
+
+  REQUIRE(tao::pegtl::parse<lua53::grammar>(in) == true);
+}
