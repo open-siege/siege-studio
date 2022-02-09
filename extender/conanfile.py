49e69591299ea99aa085f7a30e9704d33b11454f
Matthew Rindel <matthew@sobantu.co.za>
2022-02-09 14:04:11 +0200

    Updated conan files so that they don't have to create a temporary install.bat to propagate environment variables

--- extender/conanfile.py
+++ extender/conanfile.py
@@ -10,39 +10,25 @@ class DarkstarHookConan(ConanFile):
         if self.settings.arch != "x86":
             return
 
-        self.run("conan install cmake/3.22.0@/ -g virtualenv")
-        activate = "activate.bat" if self.settings.os == "Windows" else "./activate.sh"
-        install = "install.bat" if self.settings.os == "Windows" else "./install.sh"
-        copyfile(activate, install)
+        self.run(" && ".join([
+            "cd detours",
+            "conan source .",
+            "conan export . detours/4.0.1@microsoft/stable"]), run_environment=True)
+
+        self.run(" && ".join([
+            "cd darkstar",
+            "conan install . -s arch=x86 --build=missing"]), run_environment=True)
 
         # Release builds cause the dll not to work properly.
         # Tried with various optimisation levels and still has issues.
         # The debug build isn't too bad since all the main logic has been moved to darkstar.dll
-        with open(install, "a") as file:
-            commands = [
-                "cd detours",
-                f"conan source .",
-                "conan export . detours/4.0.1@microsoft/stable",
-                "cd ..",
-                "cd darkstar",
-                "conan install . -s arch=x86 --build=missing",
-                "cd ..",
-                "cd darkstar.proxy",
-                "conan install . -s arch=x86",
-                "cd ..",
-                "cd mem",
-                "conan install . -s arch=x86",
-                "cd ..",
-                "cd launcher",
-                "conan install . -s arch=x86 --build=missing",
-                "cd .."
-            ]
+        self.run(" && ".join([
+            "cd darkstar.proxy",
+            "conan install . -s arch=x86"]), run_environment=True)
 
-            file.write("\n" + " && ".join(commands))
+        self.run(" && ".join(["cd mem", "conan install . -s arch=x86"]), run_environment=True)
 
-        if self.settings.os != "Windows":
-            self.run(f"chmod +x {install}")
-        self.run(install)
+        self.run(" && ".join(["cd launcher", "conan install . -s arch=x86 --build=missing"]), run_environment=True)
 
     def build(self):
         cmake = CMake(self)
--- tools/conanfile.py
+++ tools/conanfile.py
@@ -8,24 +8,11 @@ class LocalConanFile(ConanFile):
     build_requires = "cmake/3.22.0"
 
     def requirements(self):
-        self.run("conan install cmake/3.22.0@/ -g virtualenv")
-        activate = "activate.bat" if self.settings.os == "Windows" else "./activate.sh"
-        install = "install.bat" if self.settings.os == "Windows" else "./install.sh"
-        copyfile(activate, install)
-        with open(install, "a") as file:
-            targets = ["dts-to-json", "json-to-dts", "dts-to-obj", "unvol"]
-            commands = []
+        targets = ["dts-to-json", "json-to-dts", "dts-to-obj", "unvol"]
 
-            for target in targets:
-                commands.append(f"cd {target}")
-                commands.append(f"conan install . -s build_type={self.settings.build_type} --build=missing")
-                commands.append(f"cd ..")
-
-            file.write("\n" + " && ".join(commands))
-
-        if self.settings.os != "Windows":
-            self.run(f"chmod +x {install}")
-        self.run(install)
+        for target in targets:
+            commands = [f"cd {target}", f"conan install . -s build_type={self.settings.build_type} --build=missing"]
+            self.run(" && ".join(commands), run_environment=True)
 
 
     def build(self):
