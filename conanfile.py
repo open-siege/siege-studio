b94bd49e8bbdc2574821f7c543dc0ef62b756bd3
Matthew Rindel <matthew@sobantu.co.za>
2022-02-10 14:50:46 +0200

    Image magick fixes hopefully fix the build

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,11 @@ import os.path
 import sys
 import shutil
 
+# TODO find a more permanent solution
+imagemagick_url = "https://transfer.sh/btgQI6/ImageMagick.zip"
+
 class LocalConanFile(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "compiler", "build_type", "arch", "arch_build"
     build_requires = "cmake/3.22.0"
 
     def requirements(self):
@@ -48,27 +51,35 @@ class LocalConanFile(ConanFile):
             ]
             self.run(" && ".join(commands), run_environment=True)
 
+        if not os.path.exists("ImageMagick.zip"):
+            tools.download(imagemagick_url, "ImageMagick.zip")
 
-    def build(self):
+        if not os.path.exists("ImageMagick"):
+            tools.unzip("ImageMagick.zip", "ImageMagick")
+
+
+    def _configure_cmake(self):
+        magick_home = os.path.abspath(f"ImageMagick/ImageMagick-{self.settings.arch_build}")
+        os.environ["PATH"] += os.pathsep + magick_home + os.sep
+        os.environ["MAGICK_HOME"] = magick_home
+        os.environ["MAGICK_CODER_MODULE_PATH"] = magick_home + os.sep + "modules" + os.sep + "coders"
         cmake = CMake(self)
         cmake.definitions["CONAN_CMAKE_SYSTEM_PROCESSOR"] = self.settings.arch
         cmake.definitions["CI"] = os.environ["CI"] if "CI" in os.environ else "False"
         cmake.definitions["PYTHON_EXECUTABLE"] = sys.executable
         cmake.configure(source_folder=os.path.abspath("."), build_folder=os.path.abspath("build"))
-        cmake.build()
+        return cmake
+
+    def build(self):
+        self._configure_cmake().build()
 
     def package(self):
         git = tools.Git(folder="wiki")
         git.clone("https://github.com/StarsiegePlayers/3space-studio.wiki.git", shallow=True)
         tools.rmdir("wiki/.git")
-        cmake = CMake(self)
-        cmake.definitions["CONAN_CMAKE_SYSTEM_PROCESSOR"] = self.settings.arch
-        cmake.definitions["CI"] = os.environ["CI"] if "CI" in os.environ else "False"
-        cmake.definitions["PYTHON_EXECUTABLE"] = sys.executable
-        cmake.configure(source_folder=os.path.abspath("."), build_folder=os.path.abspath("build"))
         self.copy(pattern="LICENSE", src=self.source_folder, dst="licenses")
         self.copy(pattern="README.md", src=self.source_folder, dst="res")
         self.copy(pattern="wiki/*.md", src=self.source_folder, dst="res")
         self.copy(pattern="game-support.md", src=self.source_folder, dst="res")
         tools.rmdir("wiki")
-        cmake.install()
+        self._configure_cmake().install()
