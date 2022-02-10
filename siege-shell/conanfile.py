28249920443162e37b3c6d91aeabfec36aa93988
Matthew Rindel <matthew@sobantu.co.za>
2022-02-10 11:21:31 +0200

    Added code to fetch image magic manually

--- siege-shell/conanfile.py
+++ siege-shell/conanfile.py
@@ -1,8 +1,13 @@
 from conans import ConanFile, CMake, tools
+from conans.tools import os_info, SystemPackageTool
 import glob
 import os.path
 import sys
 
+urls = {
+    "x86": "https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.1.0-23-Q16-HDRI-x86-dll.exe",
+    "x86_64": "https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.1.0-23-Q16-HDRI-x64-dll.exe"
+}
 
 class LocalConanFile(ConanFile):
     name = "siege-shell"
@@ -14,6 +19,18 @@ class LocalConanFile(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "3space/0.5.1", "wxwidgets/3.1.5@bincrafters/stable"
     generators = "cmake_find_package"
+
+    def system_requirements(self):
+        arch = str(self.settings.arch)
+        if os_info.is_windows and arch in urls:
+            url = urls[arch]
+            if not os.path.exists(f"installer-{arch}.exe"):
+                tools.download(url, f"installer-{arch}.exe")
+
+            if not os.path.exists(f"C:/ImageMagick-{arch}"):
+                self.run(f"installer-{arch}.exe /DIR=\"C:\\ImageMagick-{arch}\" /VERYSILENT /NORESTART /MERGETASKS=install_devel")
+
+
 
     def build(self):
         cmake = CMake(self)
