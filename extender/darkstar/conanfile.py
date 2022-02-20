7b07ff768f016db7cfbc2ea137cb8cac0f871731
Matthew Rindel <matthew@sobantu.co.za>
2022-02-20 10:29:56 +0200

    Created app to host wxMediaControl as wxNativeWindow will interfere with the game window if used

--- extender/darkstar/conanfile.py
+++ extender/darkstar/conanfile.py
@@ -9,33 +9,11 @@ class LocalConanFile(ConanFile):
     version = "0.5.1"
     build_requires = "cmake/3.22.0"
     settings = "os", "compiler", "build_type", "arch"
-    requires = "detours/4.0.1@microsoft/stable", "wxwidgets/3.1.5@bincrafters/stable", "nlohmann_json/3.10.5", "sfml/2.5.1", "catch2/2.13.8"
+    requires = "detours/4.0.1@microsoft/stable", "sqlite3/3.37.2", "nlohmann_json/3.10.5", "sfml/2.5.1", "catch2/2.13.8"
     generators = "cmake_find_package"
 
     def configure(self):
-        self.options["wxwidgets"].mediactrl = True
-        self.options["wxwidgets"].zlib = "off"
-        self.options["wxwidgets"].png = "off"
-        self.options["wxwidgets"].jpeg = "off"
-        self.options["wxwidgets"].tiff = "off"
-        self.options["wxwidgets"].expat = "off"
-        self.options["wxwidgets"].secretstore = False
-        self.options["wxwidgets"].aui = False
-        self.options["wxwidgets"].opengl = False
-        self.options["wxwidgets"].propgrid = False
-        self.options["wxwidgets"].ribbon = False
-        self.options["wxwidgets"].richtext = False
-        self.options["wxwidgets"].sockets = False
-        self.options["wxwidgets"].stc = False
-        self.options["wxwidgets"].richtext = False
-        self.options["wxwidgets"].webview = False
-        self.options["wxwidgets"].xml = False
-        self.options["wxwidgets"].xrc = False
-        self.options["wxwidgets"].fs_inet = False
-        self.options["wxwidgets"].help = False
-        self.options["wxwidgets"].protocol = False
-        self.options["wxwidgets"].html_help = False
-        self.options["wxwidgets"].custom_disables = "wxUSE_ARTPROVIDER_TANGO,wxUSE_SVG"
+        self.options["sqlite3"].enable_json1 = True
 
     def build(self):
         cmake = CMake(self)
---
+++ extender/tray-player/conanfile.py
@@ -0,0 +1,45 @@
+from conans import ConanFile, CMake, tools
+import os.path
+
+class HelloWxWidgetsConanFile(ConanFile):
+    build_requires = "cmake/3.22.0"
+    requires = "wxwidgets/3.1.5@bincrafters/stable", "sqlite3/3.37.2"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake_find_package"
+
+    def configure(self):
+        self.options["sqlite3"].enable_json1 = True
+        self.options["wxwidgets"].mediactrl = True
+        self.options["wxwidgets"].zlib = "off"
+        self.options["wxwidgets"].png = "off"
+        self.options["wxwidgets"].jpeg = "off"
+        self.options["wxwidgets"].tiff = "off"
+        self.options["wxwidgets"].expat = "off"
+        self.options["wxwidgets"].secretstore = False
+        self.options["wxwidgets"].aui = False
+        self.options["wxwidgets"].opengl = False
+        self.options["wxwidgets"].propgrid = False
+        self.options["wxwidgets"].ribbon = False
+        self.options["wxwidgets"].richtext = False
+        self.options["wxwidgets"].sockets = False
+        self.options["wxwidgets"].stc = False
+        self.options["wxwidgets"].richtext = False
+        self.options["wxwidgets"].webview = False
+        self.options["wxwidgets"].xml = False
+        self.options["wxwidgets"].xrc = False
+        self.options["wxwidgets"].fs_inet = False
+        self.options["wxwidgets"].help = False
+        self.options["wxwidgets"].protocol = False
+        self.options["wxwidgets"].html_help = False
+        self.options["wxwidgets"].custom_disables = "wxUSE_ARTPROVIDER_TANGO,wxUSE_SVG"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=os.path.abspath("."), build_folder=os.path.abspath("build"))
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=os.path.abspath("."), build_folder=os.path.abspath("build"))
+        cmake.install()
+
