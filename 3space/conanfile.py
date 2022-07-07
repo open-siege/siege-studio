a0a7d9f8869693b0132165ed1b09363ff3ecaf65
Matthew Rindel <matthew@sobantu.co.za>
2022-07-07 15:38:46 +0200

    Added lib zip to project

--- 3space/conanfile.py
+++ 3space/conanfile.py
@@ -11,7 +11,7 @@ class LocalConanFile(ConanFile):
     author = "Matthew Rindel (matthew@thesiegehub.com)"
     build_requires = "cmake/3.22.0"
     settings = "os", "compiler", "build_type", "arch"
-    requires = "nlohmann_json/3.10.5", "boost/1.78.0", "glm/0.9.9.8", "span-lite/0.10.3", "taocpp-pegtl/3.2.1", "catch2/2.13.8"
+    requires = "nlohmann_json/3.10.5", "boost/1.78.0", "glm/0.9.9.8", "span-lite/0.10.3", "taocpp-pegtl/3.2.1", "libzip/1.8.0", "catch2/2.13.8"
     generators = "cmake_find_package"
     exports_sources = "CMakeLists.txt", "include/*", "src/*"
 
