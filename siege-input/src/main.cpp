0c6f136b949efecf9132531d65f4305c050f16bf
Matthew Rindel <matthew@sobantu.co.za>
2022-08-11 19:30:12 +0200

    Imgui properly integrated into project

--- siege-input/src/main.cpp
+++ siege-input/src/main.cpp
@@ -1,37 +1,89 @@
 #define SDL_MAIN_HANDLED
-#include <SDL2/SDL.h>
+
+#include <imgui.h>
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_sdlrenderer.h"
 #include <iostream>
 #include <memory>
+#include <SDL.h>
 
-int main()
+int main(int, char**)
 {
-  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_JOYSTICK) != 0)
   {
-    std::cerr << "could not initialize sdl2: " << SDL_GetError() << '\n';
-    return 1;
+    std::cerr << "Error: " << SDL_GetError() << '\n';
+    return -1;
   }
 
+  // SDL scope
   {
-    auto window = std::unique_ptr<SDL_Window, void(*)(SDL_Window*)>(SDL_CreateWindow(
-                                                                       "Siege Input",
-                                                                       SDL_WINDOWPOS_UNDEFINED,
-                                                                       SDL_WINDOWPOS_UNDEFINED,
-                                                                       640,
-                                                                       480,
-                                                                       SDL_WINDOW_SHOWN), SDL_DestroyWindow);
+    SDL_WindowFlags window_flags = SDL_WindowFlags(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    auto window = std::unique_ptr<SDL_Window, void (*)(SDL_Window*)>(
+      SDL_CreateWindow("Siege Input", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags), SDL_DestroyWindow);
 
-    if (!window)
+    auto renderer = std::unique_ptr<SDL_Renderer, void (*)(SDL_Renderer*)>(SDL_CreateRenderer(window.get(), -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED), SDL_DestroyRenderer);
+    if (!renderer)
     {
-      std::cerr << "could not create window: " << SDL_GetError() << '\n';
-      return 1;
+      SDL_Log("Error creating SDL_Renderer!");
+      return -1;
     }
 
-    auto* screenSurface = SDL_GetWindowSurface(window.get());
-    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xaa, 0xbb, 0xcc));
-    SDL_UpdateWindowSurface(window.get());
-    SDL_Delay(2000);
+    // The Rapoo V600S DirectInput driver causes an access violation in winmm when this is called.
+    // Will find out if I can trigger vibration through the Joystick API alone, or if this needs some work.
+    //SDL_InitSubSystem(SDL_INIT_HAPTIC);
+
+    // ImGui scope
+    {
+      IMGUI_CHECKVERSION();
+      auto context = std::unique_ptr<ImGuiContext, void (*)(ImGuiContext*)>(ImGui::CreateContext(), [](auto* context) {
+        ImGui_ImplSDLRenderer_Shutdown();
+        ImGui_ImplSDL2_Shutdown();
+        ImGui::DestroyContext(context);
+      });
+
+      ImGuiIO& io = ImGui::GetIO();
+
+      ImGui::StyleColorsDark();
+
+      ImGui_ImplSDL2_InitForSDLRenderer(window.get(), renderer.get());
+      ImGui_ImplSDLRenderer_Init(renderer.get());
+
+      bool show_demo_window = true;
+      ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+      bool done = false;
+      while (!done)
+      {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+          ImGui_ImplSDL2_ProcessEvent(&event);
+          if (event.type == SDL_QUIT)
+            done = true;
+          if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window.get()))
+            done = true;
+        }
+
+        ImGui_ImplSDLRenderer_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        if (show_demo_window)
+        {
+          ImGui::ShowDemoWindow(&show_demo_window);
+        }
+
+        ImGui::Render();
+        SDL_SetRenderDrawColor(renderer.get(), (Uint8)(clear_color.x * 255), (Uint8)(clear_color.y * 255), (Uint8)(clear_color.z * 255), (Uint8)(clear_color.w * 255));
+        SDL_RenderClear(renderer.get());
+        ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
+        SDL_RenderPresent(renderer.get());
+      }
+    }
+    // /ImGui scope
   }
 
   SDL_Quit();
+
   return 0;
 }
