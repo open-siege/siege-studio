6c25e3ce57a92ae75ad0548987f34ad8312a7a41
Matthew Rindel <matthew@sobantu.co.za>
2022-08-11 16:43:44 +0200

    Added initial project for siege input

---
+++ siege-input/conanfile.py
@@ -0,0 +1,36 @@
+from conans import ConanFile, CMake, tools
+import glob
+import os.path
+
+class HelloImguiSfmlConanFile(ConanFile):
+    name = "siege-input"
+    version = "0.6.0"
+    url = "https://github.com/open-siege/open-siege"
+    license = "MIT"
+    author = "Matthew Rindel (matthew@thesiegehub.com)"
+    build_requires = "cmake/3.22.0"
+    requires = "imgui/cci.20220621+1.88.docking", "sdl/2.0.20"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake_find_package"
+
+    def configure(self):
+        self.options["sdl"].shared = False
+        self.options["sdl"].opengl = False
+        self.options["sdl"].opengles = False
+        self.options["sdl"].vulkan = False
+        self.options["sdl"].sdl2main = False
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=os.path.abspath("."), build_folder=os.path.abspath("build"))
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=os.path.abspath("."), build_folder=os.path.abspath("build"))
+        cmake.install()
+
+    def imports(self):
+        tools.rmdir("cmake")
+        tools.mkdir("cmake")
+        [tools.rename(file, f"cmake/{file}") for file in glob.glob("*.cmake")]
