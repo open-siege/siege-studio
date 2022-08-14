175d98b32a655af022ebc5828e48da00494b1eb9
Matthew Rindel <matthew@sobantu.co.za>
2022-08-14 15:55:09 +0200

    Virtual joysticks that are mice now show as such

--- siege-input/src/other.cpp
+++ siege-input/src/other.cpp
@@ -1,6 +1,15 @@
 #include <SDL.h>
 
-SDL_JoystickType SDLCALL Siege_JoystickGetType(SDL_Joystick *joystick);
+SDL_JoystickType Siege_JoystickGetType(SDL_Joystick *joystick)
 {
   return SDL_JoystickGetType(joystick);
+}
+
+void Siege_InitVirtualJoysticks()
+{
+}
+
+SDL_bool Siege_IsMouse(SDL_Joystick*)
+{
+  return SDL_bool::SDL_FALSE;
 }
