9c24a715b8c1c9f600a6df6f2f583c615502fad4
Matthew Rindel <matthew@sobantu.co.za>
2022-08-14 16:51:33 +0200

    Added code to get product and vendor id for mice

--- siege-input/src/other.cpp
+++ siege-input/src/other.cpp
@@ -13,3 +13,18 @@ SDL_bool Siege_IsMouse(SDL_Joystick*)
 {
   return SDL_bool::SDL_FALSE;
 }
+
+Uint16 Siege_JoystickGetVendor(SDL_Joystick *joystick)
+{
+  return SDL_JoystickGetVendor(joystick);
+}
+
+Uint16 Siege_JoystickGetProduct(SDL_Joystick *joystick)
+{
+  return SDL_JoystickGetProduct(joystick);
+}
+
+Uint16 Siege_JoystickGetProductVersion(SDL_Joystick *joystick)
+{
+  return SDL_JoystickGetProductVersion(joystick);
+}
--- siege-input/src/platform.hpp
+++ siege-input/src/platform.hpp
@@ -7,6 +7,11 @@ SDL_JoystickType Siege_JoystickGetType(SDL_Joystick* joystick);
 
 SDL_bool Siege_IsMouse(SDL_Joystick* joystick);
 
+Uint16 Siege_JoystickGetVendor(SDL_Joystick *joystick);
+Uint16 Siege_JoystickGetProduct(SDL_Joystick *joystick);
+Uint16 Siege_JoystickGetProductVersion(SDL_Joystick *joystick);
+
+
 void Siege_InitVirtualJoysticks();
 
 #endif// OPEN_SIEGE_PLATFORM_HPP
