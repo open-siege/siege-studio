2d5da9a3edf6934515084d9419640e81e5e819e0
Matthew Rindel <matthew@sobantu.co.za>
2022-11-01 18:28:43 +0200

    Special files now generated by installer

--- siege-installer/src/games/games.hpp
+++ siege-installer/src/games/games.hpp
@@ -6,6 +6,8 @@
 #include <unordered_map>
 #include <string_view>
 #include <optional>
+#include <variant>
+#include <system_error>
 
 struct game_storage_properties
 {
@@ -13,10 +15,44 @@ struct game_storage_properties
   std::array<std::string_view, 32> disc_names;
 };
 
+struct literal_template
+{
+  std::string_view value;
+};
+
+struct external_template
+{
+  std::string_view filename;
+};
+
+
+struct template_args
+{
+  const char* original_path;
+  std::size_t original_path_size;
+
+  const char* file_path;
+  std::size_t file_path_size;
+};
+
+using template_function = std::errc(template_args* args) noexcept;
+
+struct external_generated_template
+{
+  std::string_view function_name;
+};
+
+struct internal_generated_template
+{
+  template_function& generate_template;
+};
+
+using game_template = std::variant<literal_template, external_template, external_generated_template, internal_generated_template>;
+
 struct game_info
 {
   std::unordered_map<std::string_view, std::vector<std::string_view>>& variables;
-  std::vector<std::string_view>& generated_files;
+  std::unordered_map<std::string_view, game_template>& generated_files;
   std::unordered_map<std::string_view, std::string_view>& directory_mappings;
   game_storage_properties& storage_properties;
 };
