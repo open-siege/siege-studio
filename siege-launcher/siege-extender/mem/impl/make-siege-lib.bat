a63cd08e2b67c77ec59a58e4ccf6906d297a80ee
Matthew Rindel <matthew@thesiegehub.com>
2023-06-08 08:29:54 +0200

    Goodbye siege extender

--- siege-launcher/siege-extender/mem/impl/make-siege-lib.bat
+++
@@ -1,2 +0,0 @@
-mkdir libs
-lib /def:..\..\siege\src\siege.def /out:libs\siege.lib /machine:x86
--- siege-launcher/siege-extender/scripts/flyer/keyMaps/flyer_Keyboard_Mouse.cs
+++
@@ -1,6 +0,0 @@
-exec("Keyboard_Mouse.cs");
-
-editActionMap(Herc);
-exec("flyer\\flyerKeyBindings.cs");
-bindAction( mouse0, xaxis,   TO, IDACTION_YAW, scale, 0.25, flip ); 
-bindAction( mouse0, yaxis,   TO, IDACTION_PITCH, scale, 0.25, flip ); 
