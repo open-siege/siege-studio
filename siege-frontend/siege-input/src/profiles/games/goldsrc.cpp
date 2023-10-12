98e34f2e9c629b2f9d0315abd2393177501b0934
Matthew Rindel <matthew@sobantu.co.za>
2023-10-12 06:30:19 +0200

    Added new configs for multiple id_tech games

---
+++ siege-frontend/siege-input/src/profiles/games/goldsrc.cpp
@@ -0,0 +1,17 @@
+
+// 007 Nightfire
+// bond/config.cfg
+
+// Half Life Day One
+
+// Gunman Chronicles
+
+// Half Life (Steam)
+
+// Counter Strike (Steam)
+
+// Day of Defeat (Steam)
+
+// Ricochet (Steam)
+
+// Deathmatch Classic (Steam)
---
+++ siege-frontend/siege-input/src/profiles/games/id_tech_2_0.cpp
@@ -0,0 +1,8 @@
+// FreeCS
+// OpenHL
+// Quake (DOS)
+// WinQuake
+// GLQuake
+// Hexen 2
+// CIA Operatives
+// Laser Arena
