8821c8e715fc8f0b73bd13f6410fd3edea460b5b
Matthew Rindel <matthew@thesiegehub.com>
2024-01-23 13:10:04 +0100

    Win32 type improvements

--- siege-frontend/siege-launcher-win32/windows.hpp
+++ siege-frontend/siege-launcher-win32/windows.hpp
@@ -7,6 +7,7 @@
 #include <functional>
 #include <variant>
 #include <optional>
+#include <bit>
 
 static_assert(sizeof(void*) == sizeof(LONG_PTR));
 
@@ -71,35 +72,37 @@ using window_message = std::variant<message,
 	init_dialog_message,
 	destroy_message>;
 
-window_message make_window_message(UINT message, WPARAM wParam, LPARAM lParam) noexcept
+static_assert(sizeof(window_message) < sizeof(MSG));
+
+constexpr window_message make_window_message(UINT message, WPARAM wParam, LPARAM lParam) noexcept
 {
 	if (message == command_message::id)
 	{
 		return command_message{
 			.notification_code = HIWORD(wParam),
 			.identifier = LOWORD(wParam),
-			.handle = reinterpret_cast<HWND>(lParam)
+			.handle = std::bit_cast<HWND>(lParam)
 		};
 	}
 
 	if (message == pre_create_message::id)
 	{
 		return pre_create_message{
-			.data = *reinterpret_cast<CREATESTRUCTW*>(lParam)
+			.data = *std::bit_cast<CREATESTRUCTW*>(lParam)
 		};
 	}
 
 	if (message == create_message::id)
 	{
 		return create_message{
-			.data = *reinterpret_cast<CREATESTRUCTW*>(lParam)
+			.data = *std::bit_cast<CREATESTRUCTW*>(lParam)
 		};
 	}
 
 	if (message == init_dialog_message::id)
 	{
 		return init_dialog_message{
-			.default_focus_control = reinterpret_cast<HWND>(wParam)
+			.default_focus_control = std::bit_cast<HWND>(wParam)
 		};
 	}
 
@@ -115,55 +118,64 @@ window_message make_window_message(UINT message, WPARAM wParam, LPARAM lParam) n
 	};
 }
 
-window_message make_window_message(const MSG& message) noexcept
+constexpr window_message make_window_message(const MSG& message) noexcept
 {
 	return make_window_message(message.message, message.wParam, message.lParam);
 }
 
-struct main_window
+struct window
 {
-	std::function<std::optional<LRESULT>(main_window&, window_message)> HandleMessage;
+	using callback_type = std::move_only_function<std::optional<LRESULT>(window&, window_message)>;
+	
 	HWND handle;
+	callback_type HandleMessage;
+	
 
-	static auto get_descriptor(HINSTANCE hInstance)
-	{
-		constexpr static auto descriptor = WNDCLASSEXW{
-		.cbSize{sizeof(WNDCLASSEX)},
-		.style{CS_HREDRAW | CS_VREDRAW},
-		.lpfnWndProc{WindowHandler},
-		.cbClsExtra{0},
-		.cbWndExtra{0},
-		.lpszClassName{L"SiegeLauncherMainWindow"}
-		};
-		auto temp = descriptor;
-		temp.hInstance = hInstance;
-		temp.hIcon = LoadIconW(hInstance, MAKEINTRESOURCE(IDI_SIEGELAUNCHERWIN32));
-		temp.hCursor = LoadCursorW(hInstance, IDC_ARROW);
-		temp.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-		temp.lpszMenuName = MAKEINTRESOURCEW(IDC_SIEGELAUNCHERWIN32);
-		temp.hIconSm = LoadIconW(hInstance, MAKEINTRESOURCE(IDI_SMALL));
-		return temp;
-	}
+	window(WNDCLASSEXW descriptor, CREATESTRUCTW params, callback_type handler) 
+		: handle(0),
+		HandleMessage(std::move(handler))
+		{
+			descriptor.cbSize = sizeof(WNDCLASSEXW);
+			descriptor.lpfnWndProc = WindowHandler;
+			
+			if (GetClassInfoExW(descriptor.hInstance, descriptor.lpszClassName, &descriptor) == FALSE)
+			{
+				RegisterClassExW(&descriptor);
+			}
+
+			handle = CreateWindowExW(params.dwExStyle,
+				descriptor.lpszClassName,
+				params.lpszName,
+				params.style,
+				params.x,
+				params.y,
+				params.cx,
+				params.cy,
+				params.hwndParent,
+				params.hMenu,
+				descriptor.hInstance,
+				std::bit_cast<LPVOID>(this));
+		}
 
 	static LRESULT CALLBACK WindowHandler(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	{
-		main_window* window = nullptr;
+		window* self = nullptr;
 
 		if (message == WM_NCCREATE)
 		{
-			auto* pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
-			window = reinterpret_cast<main_window*>(pCreate->lpCreateParams);
-			SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(window));
+			auto* pCreate = std::bit_cast<CREATESTRUCT*>(lParam);
+			self = std::bit_cast<window*>(pCreate->lpCreateParams);
+			SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(self));
+			self->handle = hWnd;
 		}
 		else
 		{
-			window = reinterpret_cast<main_window*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+			self = std::bit_cast<window*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
 		}
 
-		if (window && window->HandleMessage)
+		if (self && self->HandleMessage)
 		{
-			window->handle = hWnd;
-			auto result = window->HandleMessage(*window, make_window_message(message, wParam, lParam));
+			auto result = self->HandleMessage(*self, make_window_message(message, wParam, lParam));
 
 			if (result.has_value())
 			{
@@ -178,8 +190,21 @@ struct main_window
 
 struct dialog
 {
-	std::function<INT_PTR(dialog&, window_message)> HandleMessage;
+	using callback_type = std::move_only_function<INT_PTR(dialog&, window_message)>;
 	HWND handle;
+	callback_type HandleMessage;
+	
+	dialog(callback_type handler): handle(0), HandleMessage(std::move(handler))
+	{
+	}
+
+	[[maybe_unused]] static auto show_modal(HWND parent, LPWSTR templateName, callback_type handler)
+	{
+		dialog temp{std::move(handler)};
+		auto hInstance = std::bit_cast<HINSTANCE>(GetWindowLongPtrW(parent, GWLP_HINSTANCE));
+		return DialogBoxParamW(hInstance, templateName, parent, dialog::HandleAboutDialogMessage, std::bit_cast<LPARAM>(&temp));
+	}
+	
 
 	static INT_PTR CALLBACK HandleAboutDialogMessage(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	{
@@ -187,23 +212,28 @@ struct dialog
 
 		if (message == WM_INITDIALOG)
 		{
-			child = reinterpret_cast<dialog*>(lParam);
-			SetWindowLongPtr(hDlg, DWLP_USER, reinterpret_cast<LONG_PTR>(child));
+			child = std::bit_cast<dialog*>(lParam);
+			
+			if (child->handle == 0)
+			{
+	            child->handle = hDlg;
+			}
+
+			SetWindowLongPtr(hDlg, DWLP_USER, std::bit_cast<LONG_PTR>(child));
 			return (INT_PTR)TRUE;
 		}
 		else
 		{
-			child = reinterpret_cast<dialog*>(GetWindowLongPtr(hDlg, DWLP_USER));
+			child = std::bit_cast<dialog*>(GetWindowLongPtr(hDlg, DWLP_USER));
 		}
 
 		if (child && child->HandleMessage)
 		{
-			child->handle = hDlg;
 			auto result = child->HandleMessage(*child, make_window_message(message, wParam, lParam));
 
 			if (message == WM_DESTROY)
 			{
-				SetWindowLongPtr(child->handle, DWLP_USER, reinterpret_cast<LONG_PTR>(nullptr));
+				SetWindowLongPtr(child->handle, DWLP_USER, std::bit_cast<LONG_PTR>(nullptr));
 				child->HandleMessage = nullptr;
 				child->handle = 0;
 				return (INT_PTR)TRUE;
@@ -221,7 +251,7 @@ struct control
 {
 	HWND handle;
 
-	using callback_type = std::function<std::optional<LRESULT>(Control&, UINT_PTR uIdSubclass, window_message)>;
+	using callback_type = std::move_only_function<std::optional<LRESULT>(Control&, UINT_PTR uIdSubclass, window_message)>;
 	callback_type HandleMessage;
 
 	static LRESULT CALLBACK CustomButtonHandler(HWND hWnd, UINT uMsg, WPARAM wParam,
@@ -253,7 +283,25 @@ struct control
 	control(HWND handle, callback_type handler)
 		: handle(handle), HandleMessage(std::move(handler))
 	{
-		SetWindowSubclass(handle, control::CustomButtonHandler, 0, reinterpret_cast<DWORD_PTR>(this));
+		SetWindowSubclass(handle, control::CustomButtonHandler, 0, std::bit_cast<DWORD_PTR>(this));
+	}
+
+	control(CREATESTRUCTW params, callback_type handler)
+		: control(CreateWindowExW(
+			params.dwExStyle,
+			params.lpszClass ? params.lpszClass : Control::class_name,
+			params.lpszName,
+			params.style,
+			params.x,
+			params.y,
+			params.cx,
+			params.cy,
+			params.hwndParent,
+			params.hMenu,
+			params.hInstance != 0 ? params.hInstance : std::bit_cast<HINSTANCE>(GetWindowLongPtrW(params.hwndParent, GWLP_HINSTANCE)),
+			params.lpCreateParams
+		), std::move(handler)) 
+	{
 	}
 
 	control(const control&) = delete;
@@ -261,7 +309,10 @@ struct control
 	control(control&& other) noexcept
 		: control(other.handle, std::move(other.HandleMessage))
 	{
-		SetWindowSubclass(handle, control::CustomButtonHandler, 0, reinterpret_cast<DWORD_PTR>(this));
+		if (IsWindow(handle))
+		{
+			SetWindowSubclass(handle, control::CustomButtonHandler, 0, std::bit_cast<DWORD_PTR>(this));
+		}
 	}
 
 	control& operator=(control&& other) noexcept
@@ -275,31 +326,43 @@ struct control
 struct button : control<button>
 {
 	using control::control;
+
+	constexpr static auto class_name = WC_BUTTONW;
 };
 
 struct combo_box : control<combo_box>
 {
 	using control::control;
+
+	constexpr static auto class_name = WC_COMBOBOXW;
 };
 
 struct edit : control<edit>
 {
 	using control::control;
+
+	constexpr static auto class_name = WC_EDITW;
 };
 
 struct list_box : control<list_box>
 {
 	using control::control;
+
+	constexpr static auto class_name = WC_LISTBOXW;
 };
 
 struct scroll_bar : control<scroll_bar>
 {
 	using control::control;
+	
+	constexpr static auto class_name = WC_SCROLLBARW;
 };
 
 struct static_text : control<static_text>
 {
 	using control::control;
+
+	constexpr static auto class_name = WC_STATICW;
 };
 
 using client_control = std::variant<button, combo_box, edit, list_box, scroll_bar, static_text>;
