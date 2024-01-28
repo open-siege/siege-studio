888651a000ce44bb5937ec6f4b97c828e606aa09
Matthew Rindel <matthew@sobantu.co.za>
2024-01-28 08:53:43 +0100

    Updated xml factory to map file data

--- siege-frontend/siege-launcher-win32/xml_factory.hpp
+++ siege-frontend/siege-launcher-win32/xml_factory.hpp
@@ -7,19 +7,28 @@
 #include <combaseapi.h>
 #include <msxml6.h>
 
+template<typename T>
+auto make_unique(T* value, void(*deleter)(T*))
+{
+	return std::unique_ptr<T, void(*)(T*)>(value, deleter);
+}
+
 std::vector<std::any> load_data(std::filesystem::path path)
 {
 	HRESULT result;
-	IXMLDOMDocument* document;
 
-	result = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	result = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	void* raw = nullptr;
+	result = CoCreateInstance(CLSID_DOMDocument60, nullptr, CLSCTX_INPROC_SERVER, IID_IXMLDOMDocument3, &raw);
 
-	result = CoCreateInstance(CLSID_DOMDocument60, NULL, CLSCTX_INPROC_SERVER, IID_IXMLDOMDocument3, reinterpret_cast<void**>(&document));
+	auto document = make_unique<IXMLDOMDocument3>(reinterpret_cast<IXMLDOMDocument3*>(raw), [](IXMLDOMDocument3* self) { if (self) self->Release(); });
+	auto mapping_handle = make_unique<void>(OpenFileMappingW(FILE_MAP_READ, FALSE, path.c_str()), [](HANDLE base) {CloseHandle(base);});
+	auto file_data = make_unique<void>(MapViewOfFile(mapping_handle.get(), FILE_MAP_READ, 0, 0, 0), [](void* base) {UnmapViewOfFile(base);});
 
-	BSTR data = nullptr;
+	auto unparsed_data = make_unique<wchar_t>(SysAllocString(reinterpret_cast<wchar_t*>(file_data.get())), [](wchar_t* data) { SysFreeString(data); });
 
 	VARIANT_BOOL success;
-	result = document->loadXML(data, &success);
+	result = document->loadXML(unparsed_data.get(), &success);
 
 	return {};
 }
