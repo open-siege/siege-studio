4543065e7bd21ba4c4a2635c128eecaca3f3f775
Matthew Rindel <matthew@sobantu.co.za>
2024-01-28 16:42:37 +0100

    Updated xml factory to use thread local for com initializer Updated load data to use expected

--- siege-frontend/siege-launcher-win32/xml_factory.hpp
+++ siege-frontend/siege-launcher-win32/xml_factory.hpp
@@ -5,6 +5,7 @@
 #include <string_view>
 #include <filesystem>
 #include <memory>
+#include <expected>
 #include <combaseapi.h>
 #include <msxml6.h>
 
@@ -15,39 +16,50 @@ auto make_unique(T* value, void(*deleter)(T*))
 }
 
 
-std::vector<std::any> load_data(std::unique_ptr<wchar_t, void(*)(wchar_t*)> xml_data)
+HRESULT init_com()
 {
-	HRESULT result;
-
-	result = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
-
-	auto com_handle = make_unique<HRESULT>(&result, [](auto*){ CoUninitialize(); });
-
-	{
-		void* raw = nullptr;
-		result = CoCreateInstance(CLSID_DOMDocument60, nullptr, CLSCTX_INPROC_SERVER, IID_IXMLDOMDocument3, &raw);
-
-		auto document = make_unique<IXMLDOMDocument3>(reinterpret_cast<IXMLDOMDocument3*>(raw), [](auto* self) { if (self) self->Release(); });
-		VARIANT_BOOL success;
-		result = document->loadXML(xml_data.get(), &success);
-	}
-
-	return {};
+	thread_local HRESULT result = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	thread_local auto com_handle = make_unique<HRESULT>(&result, [](auto*){ CoUninitialize(); });
+	return result;
 }
 
-std::vector<std::any> load_data(std::string_view xml_data)
+
+std::expected<IXMLDOMDocument3*, HRESULT> load_data(std::unique_ptr<wchar_t, void(*)(wchar_t*)> xml_data)
+{
+	HRESULT result = init_com();
+	void* raw = nullptr;
+	result = CoCreateInstance(CLSID_DOMDocument60, nullptr, CLSCTX_INPROC_SERVER, IID_IXMLDOMDocument3, &raw);
+
+	auto document = make_unique<IXMLDOMDocument3>(reinterpret_cast<IXMLDOMDocument3*>(raw), [](auto* self) { if (self) self->Release(); });
+	VARIANT_BOOL success;
+	result = document->loadXML(xml_data.get(), &success);
+
+	if (result != S_OK)
+	{
+		return std::unexpected(result);
+	}
+
+	if (success == VARIANT_FALSE)
+	{
+		return std::unexpected(S_FALSE);
+	}
+
+	return document.release();
+}
+
+auto load_data(std::string_view xml_data)
 {
 	auto raw_string = make_unique<wchar_t>(SysAllocStringByteLen(xml_data.data(), xml_data.size()), [](wchar_t* data) { SysFreeString(data); });
 	return load_data(std::move(raw_string));
 }
 
-std::vector<std::any> load_data(std::wstring_view xml_data)
+auto load_data(std::wstring_view xml_data)
 {
 	auto raw_string = make_unique<wchar_t>(SysAllocStringLen(xml_data.data(), xml_data.size()), [](wchar_t* data) { SysFreeString(data); });
 	return load_data(std::move(raw_string));
 }
 
-std::vector<std::any> load_data(std::filesystem::path path)
+auto load_data(std::filesystem::path path)
 {
 	auto file_size = std::filesystem::file_size(path);
 	auto mapping_handle = make_unique<void>(OpenFileMappingW(FILE_MAP_READ, FALSE, path.c_str()), [](auto base) {CloseHandle(base);});
