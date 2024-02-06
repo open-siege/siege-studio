1f8c100c84391fc384fa81ca1e6452036671bf2a
Matthew Rindel <matthew@thesiegehub.com>
2024-02-06 11:32:03 +0100

    Added registration code to siege bmp

--- siege-frontend/siege-modules/siege-bmp/src/main.cpp
+++ siege-frontend/siege-modules/siege-bmp/src/main.cpp
@@ -1,6 +1,5 @@
 #include <win32_controls.hpp>
 
-
 struct bitmap_window
 {
     constexpr static std::u8string_view formats = u8".jpg .jpeg .gif .png .tag .bmp .dib .pba .dmb .db0 .db1 .db2 .hba .hb0 .hb1 .hb2";
@@ -50,9 +49,9 @@ struct module_info
 
     module_info(win32::hwnd_t self, const CREATESTRUCTW&)
     {
-        SetPropW(self, L"bitmap_window", bitmap_window::formats.data());
-        SetPropW(self, L"pal_window", pal_window::formats.data());
-        SetPropW(self, L"pal_mapping_window", pal_mapping_window::formats.data());
+        SetPropA(self, typeid(bitmap_window).name(), bitmap_window::formats.data());
+        SetPropA(self, typeid(pal_window).name(), pal_window::formats.data());
+        SetPropA(self, typeid(pal_mapping_window).name(), pal_mapping_window::formats.data());
 
         SetPropW(self, L"All Images", bitmap_window::formats.data());
         SetPropW(self, L"All Palettes", pal_window::formats.data());
@@ -91,6 +90,16 @@ extern "C" BOOL WINAPI DllMain(
     switch( fdwReason ) 
     { 
         case DLL_PROCESS_ATTACH:
+
+        win32::RegisterClassExW<bitmap_window>(WNDCLASSEXW{});
+        win32::RegisterClassExW<pal_window>(WNDCLASSEXW{});
+        win32::RegisterClassExW<pal_mapping_window>(WNDCLASSEXW{});
+
+        win32::RegisterStaticClassExW<module_info>(WNDCLASSEXW{});
+
+        static auto info_instance = win32::CreateWindowExW(CREATESTRUCTW{
+            .hwndParent = HWND_MESSAGE
+        });
          // Initialize once for each new process.
          // Return FALSE to fail DLL load.
             break;
@@ -109,9 +118,16 @@ extern "C" BOOL WINAPI DllMain(
             {
                 break; // do not do cleanup if process termination scenario
             }
+
+        DestroyWindow(info_instance);
+        win32::UnregisterClassW<module_info>(hinstDLL);
+
+        win32::UnregisterClassW<bitmap_window>(hinstDLL);
+        win32::UnregisterClassW<pal_window>(hinstDLL);
+        win32::UnregisterClassW<pal_mapping_window>(hinstDLL);
             
          // Perform any necessary cleanup.
-            break;
+        break;
     }
     return TRUE;  // Successful DLL_PROCESS_ATTACH.
 }
