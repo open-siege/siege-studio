6f56260a9f85b250dbeaf85f50ac76835861f727
Matthew Rindel <matthew@thesiegehub.com>
2024-02-06 08:07:58 +0100

    Initial structure of view module

--- siege-frontend/siege-modules/siege-bmp/src/main.cpp
+++ siege-frontend/siege-modules/siege-bmp/src/main.cpp
@@ -1,5 +1,83 @@
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+#include <win32_controls.hpp>
+
+
+struct bitmap_window
+{
+    constexpr static std::u8string_view formats = u8".jpg .jpeg .gif .png .tag .bmp .dib .pba .dmb .db0 .db1 .db2 .hba .hb0 .hb1 .hb2";
+
+    static bool is_bitmap(std::istream& raw_data)
+    {
+
+    }
+};
+
+struct pal_window
+{
+    constexpr static std::u8string_view formats = ".pal .ipl .ppl .dpl";
+
+    static bool is_pal(std::istream& raw_data)
+    {
+        std::array<std::byte, 4> header{};
+        studio::read(raw_data, header.data(), sizeof(header));
+
+        raw_data.seekg(-int(sizeof(header)), std::ios::cur);
+
+        return header == dba_tag;
+    }
+};
+
+struct pal_mapping_window
+{
+    constexpr static std::u8string_view formats = "palettes.settings.json";
+};
+
+
+struct is_supported_message
+{
+    enum class handle_type
+    {
+        path,
+        file,
+        view
+    } type;
+
+    int size;
+};
+
+struct module_info
+{
+    std::uint32_t is_supported_id;
+
+    module_info(win32::hwnd_t self, const CREATESTRUCTW&)
+    {
+        SetPropW(self, L"bitmap_window", bitmap_window::formats.data());
+        SetPropW(self, L"pal_window", pal_window::formats.data());
+        SetPropW(self, L"pal_mapping_window", pal_mapping_window::formats.data());
+
+        SetPropW(self, L"All Images", bitmap_window::formats.data());
+        SetPropW(self, L"All Palettes", pal_window::formats.data());
+
+        is_supported_id = RegisterWindowMessageW(L"is_supported_message");
+    }
+
+    std::optional<win32::lresult_t> on_is_supported(is_supported_message message)
+    {
+        if (message.type == is_supported_message::handle_type::path)
+        {
+            return FALSE;
+        }
+        else if (message.type == is_supported_message::handle_type::file)
+        {
+            return FALSE;
+        }
+        else if (message.type == is_supported_message::handle_type::view)
+        {
+            return FALSE;
+        }
+    }
+};
+
+
 
 __declspec(dllexport) auto* name = "siege-bmp"; 
 
