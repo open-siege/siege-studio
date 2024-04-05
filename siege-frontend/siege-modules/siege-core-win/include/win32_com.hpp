9bec73f24652f403a4f8742e1c2821c8288c3e20
Matthew Rindel <matthew@sobantu.co.za>
2024-04-05 07:25:52 +0200

    Added com deleter

--- siege-frontend/siege-modules/siege-core-win/include/win32_com.hpp
+++ siege-frontend/siege-modules/siege-core-win/include/win32_com.hpp
@@ -6,21 +6,30 @@
 
 namespace win32::com
 {
+	template <typename T>
+	constexpr auto get_com_deleter()
+	{
+		return [](T* value) {
+			if constexpr(std::is_same_v<IUnknown, T> || std::is_base_of_v<IUnknown, T>)
+            {
+				if (value)
+				{
+					value->Release();
+				}
+            }
+		};
+	}
+
+	template<typename T>
+	constexpr auto as_unique(T* value)
+	{
+		return std::unique_ptr<T, decltype(get_com_deleter<T>())>(value, get_com_deleter<T>());
+	}
+
 	template<typename T>
 	auto as_unique(T* value, void(*deleter)(T*))
 	{
 		return std::unique_ptr<T, void(*)(T*)>(value, deleter);
-	}
-
-	template<typename T>
-	auto as_unique(T* value)
-	{
-		return std::unique_ptr<T, void(*)(T*)>(value, [](auto* self) { 
-			if (self) 
-			{
-				self->Release();
-			}
-		});
 	}
 
 	HRESULT init_com();
