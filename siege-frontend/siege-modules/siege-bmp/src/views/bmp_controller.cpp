ce66eec40f93f515659b0be9e78340b0eb5922db
Matthew Rindel <matthew@sobantu.co.za>
2024-04-16 06:53:07 +0200

    Bmp view opens

--- siege-frontend/siege-modules/siege-bmp/src/views/bmp_controller.cpp
+++ siege-frontend/siege-modules/siege-bmp/src/views/bmp_controller.cpp
@@ -1,9 +1,13 @@
 #include "bmp_controller.hpp"
+#include "content/bmp/bitmap.hpp"
+
 
 namespace siege::views
 {
   bool bmp_controller::is_bmp(std::istream& image_stream)
   {
-      return false;
+      return studio::content::bmp::is_earthsiege_bmp(image_stream)
+           || studio::content::bmp::is_microsoft_bmp(image_stream)
+          || studio::content::bmp::is_phoenix_bmp(image_stream);
   }
 }
