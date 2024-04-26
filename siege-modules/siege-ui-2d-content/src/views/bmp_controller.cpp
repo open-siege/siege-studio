a04d986e6595f7b301751b6d5c96a0a42b8a5e06
Matthew Rindel <matthew@sobantu.co.za>
2024-04-26 17:53:18 +0200

    Added logic to bmp controller

--- siege-modules/siege-ui-2d-content/src/views/bmp_controller.cpp
+++ siege-modules/siege-ui-2d-content/src/views/bmp_controller.cpp
@@ -13,6 +13,23 @@ namespace siege::views
 
   std::size_t bmp_controller::load_bitmap(std::istream& image_stream)
   {
-    return 0;
+      using namespace siege::content;
+
+      auto start = image_stream.tellg();
+
+      if (bmp::is_microsoft_bmp(image_stream))
+      {
+        raw_image = bmp::get_bmp_data(image_stream);
+      }
+      else if (bmp::is_phoenix_bmp(image_stream))
+      {
+        raw_image = bmp::get_pbmp_data(image_stream);
+      }
+      else if (bmp::is_earthsiege_bmp(image_stream))
+      {
+        raw_image = bmp::read_earthsiege_bmp(image_stream);
+      }
+
+     return image_stream.tellg() - start;
   }
 }
