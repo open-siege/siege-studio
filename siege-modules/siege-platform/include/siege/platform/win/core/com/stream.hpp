17afe580a885753789d8dd399f9e0e58f9897ff7
Matthew Rindel <matthew@sobantu.co.za>
2024-06-02 07:03:17 +0200

    Implemented stream write with tests

--- siege-modules/siege-platform/include/siege/platform/win/core/com/stream.hpp
+++ siege-modules/siege-platform/include/siege/platform/win/core/com/stream.hpp
@@ -11,20 +11,20 @@ namespace win32::com
 {
 	class StdStreamRef : public ComObject, public ::IStream
 	{
-		std::istream& stream;
+		std::iostream& stream;
 
 	public:
-		StdStreamRef(std::istream& stream) : stream(stream)
-		{		
+		StdStreamRef(std::iostream& stream) : stream(stream)
+		{
 		}
 
 		HRESULT __stdcall QueryInterface(const GUID& riid, void** ppvObj) noexcept override
-        {
-            return ComQuery<IUnknown, ::IStream>(*this, riid, ppvObj)
-                .or_else([&]() { return ComQuery<::ISequentialStream>(*this, riid, ppvObj); })
-                .or_else([&]() { return ComQuery<::IStream>(*this, riid, ppvObj); })
-                .value_or(E_NOINTERFACE);
-        }
+		{
+			return ComQuery<IUnknown, ::IStream>(*this, riid, ppvObj)
+				.or_else([&]() { return ComQuery<::ISequentialStream>(*this, riid, ppvObj); })
+				.or_else([&]() { return ComQuery<::IStream>(*this, riid, ppvObj); })
+				.value_or(E_NOINTERFACE);
+		}
 
 		[[maybe_unused]] ULONG __stdcall AddRef() noexcept override
 		{
@@ -36,27 +36,53 @@ namespace win32::com
 			return ComObject::Release();
 		}
 
-		HRESULT __stdcall Read(void* data, ULONG size, ULONG* values_read) override
+		HRESULT __stdcall Read(void* data, ULONG size, ULONG* data_read) override
 		{
-			if (data == nullptr || values_read == nullptr)
+			if (data == nullptr || data_read == nullptr)
 			{
 				return STG_E_INVALIDPOINTER;
 			}
 
 			if (size == 0)
 			{
-				*values_read = 0;
+				*data_read = 0;
 				return S_OK;
 			}
 
-			*values_read = stream.read((char*)data, size).gcount();
+			*data_read = stream.read((char*)data, size).gcount();
 
-			return size == *values_read ? S_OK : S_FALSE;
+			return size == *data_read ? S_OK : S_FALSE;
 		}
 
-		HRESULT __stdcall Write(const void*, ULONG, ULONG*) override
+		HRESULT __stdcall Write(const void* data, ULONG size, ULONG* data_written) override
 		{
-			return E_NOTIMPL;
+			if (data == nullptr)
+			{
+				return STG_E_INVALIDPOINTER;
+			}
+
+			if (size == 0)
+			{
+				if (data_written)
+				{
+					*data_written = 0;
+				}
+				return S_OK;
+			}
+
+			stream.write((const char*)data, size);
+
+			if (stream.bad())
+			{
+				return STG_E_WRITEFAULT;
+			}
+
+			if (data_written)
+			{
+				*data_written = size;
+			}
+
+			return S_OK;
 		}
 
 		HRESULT __stdcall Clone(IStream**) override
--- siege-modules/siege-platform/src/com-stream-tests.cpp
+++ siege-modules/siege-platform/src/com-stream-tests.cpp
@@ -2,14 +2,14 @@
 #include <sstream>
 #include <siege/platform/win/core/com/stream.hpp>
 
-TEST_CASE("When StdStreamRef.Read has null parameters, the error code is invalid pointer.", "[win.core.com.stream]")
+TEST_CASE("When StdStreamRef.Read has null parameters, the return code is invalid pointer.")
 {
 	std::stringstream storage;
 	win32::com::StdStreamRef test(storage);
 	REQUIRE(test.Read(nullptr, 0, nullptr) == STG_E_INVALIDPOINTER);
 }
 
-TEST_CASE("When StdStreamRef.Read points to empty storage, the byte written count is zero.", "[win.core.com.stream]")
+TEST_CASE("When StdStreamRef.Read points to empty storage, the byte written count is zero.")
 {
 	std::stringstream storage;
 	win32::com::StdStreamRef test(storage);
@@ -21,7 +21,7 @@ TEST_CASE("When StdStreamRef.Read points to empty storage, the byte written coun
 	REQUIRE(read == 0);
 }
 
-TEST_CASE("When StdStreamRef.Read points to empty storage, the return code is false.", "[win.core.com.stream]")
+TEST_CASE("When StdStreamRef.Read points to empty storage, the return code is false.")
 {
 	std::stringstream storage;
 	win32::com::StdStreamRef test(storage);
@@ -31,7 +31,7 @@ TEST_CASE("When StdStreamRef.Read points to empty storage, the return code is fa
 	REQUIRE(test.Read(buffer.data(), buffer.size(), &read) == S_FALSE);
 }
 
-TEST_CASE("When StdStreamRef.Read has an empty buffer, the byte written count is zero.", "[win.core.com.stream]")
+TEST_CASE("When StdStreamRef.Read has an empty buffer, the byte written count is zero.")
 {
 	std::stringstream storage;
 	win32::com::StdStreamRef test(storage);
@@ -42,7 +42,7 @@ TEST_CASE("When StdStreamRef.Read has an empty buffer, the byte written count is
 	REQUIRE(read == 0);
 }
 
-TEST_CASE("When StdStreamRef.Read has an empty buffer, the return code is ok.", "[win.core.com.stream]")
+TEST_CASE("When StdStreamRef.Read has an empty buffer, the return code is ok.")
 {
 	std::stringstream storage;
 	win32::com::StdStreamRef test(storage);
@@ -53,7 +53,7 @@ TEST_CASE("When StdStreamRef.Read has an empty buffer, the return code is ok.", 
 }
 
 
-TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and all bytes are requests, the byte written matches the total size.", "[win.core.com.stream]")
+TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and all bytes are requests, the byte written matches the total size.")
 {
 	std::stringstream storage("Hello World:");
 	win32::com::StdStreamRef test(storage);
@@ -64,7 +64,7 @@ TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and all bytes are 
 	REQUIRE(read == buffer.size());
 }
 
-TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and all bytes are requested, the return code is ok.", "[win.core.com.stream]")
+TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and all bytes are requested, the return code is ok.")
 {
 	std::stringstream storage("Hello World:");
 	win32::com::StdStreamRef test(storage);
@@ -74,7 +74,7 @@ TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and all bytes are 
 	REQUIRE(test.Read(buffer.data(), buffer.size(), &read) == S_OK);
 }
 
-TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and all bytes are requested, the value read matches the original value.", "[win.core.com.stream]")
+TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and all bytes are requested, the value read matches the original value.")
 {
 	std::stringstream storage("Hello World:");
 	win32::com::StdStreamRef test(storage);
@@ -85,7 +85,7 @@ TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and all bytes are 
 	REQUIRE(std::memcmp(buffer.data(), "Hello World:", 12) == 0);
 }
 
-TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and 5 bytes are requested, the bytes read matches the requested size.", "[win.core.com.stream]")
+TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and 5 bytes are requested, the bytes read matches the requested size.")
 {
 	std::stringstream storage("Hello World:");
 	win32::com::StdStreamRef test(storage);
@@ -96,7 +96,7 @@ TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and 5 bytes are re
 	REQUIRE(read == buffer.size());
 }
 
-TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and 5 bytes are requested, the return code is ok.", "[win.core.com.stream]")
+TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and 5 bytes are requested, the return code is ok.")
 {
 	std::stringstream storage("Hello World:");
 	win32::com::StdStreamRef test(storage);
@@ -106,7 +106,7 @@ TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and 5 bytes are re
 	REQUIRE(test.Read(buffer.data(), buffer.size(), &read) == S_OK);
 }
 
-TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and 5 bytes are requested, the value read matches the original value.", "[win.core.com.stream]")
+TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and 5 bytes are requested, the value read matches the original value.")
 {
 	std::stringstream storage("Hello World:");
 	win32::com::StdStreamRef test(storage);
@@ -115,4 +115,100 @@ TEST_CASE("When StdStreamRef.Read has 12 bytes in its storage and 5 bytes are re
 
 	test.Read(buffer.data(), buffer.size(), &read);
 	REQUIRE(std::memcmp(buffer.data(), "Hello", 5) == 0);
+}
+
+TEST_CASE("When StdStreamRef.Write has null parameters, the return code is invalid pointer.")
+{
+	std::stringstream storage;
+	win32::com::StdStreamRef test(storage);
+	REQUIRE(test.Write(nullptr, 0, nullptr) == STG_E_INVALIDPOINTER);
+}
+
+TEST_CASE("When StdStreamRef.Write has an empty buffer, the return code is okay.")
+{
+	std::stringstream storage;
+	win32::com::StdStreamRef test(storage);
+	std::array<std::byte, 1> buffer{};
+
+	REQUIRE(test.Write(buffer.data(), 0, nullptr) == S_OK);
+}
+
+TEST_CASE("When StdStreamRef.Write has an empty buffer, with a counter, the return code is okay.")
+{
+	std::stringstream storage;
+	win32::com::StdStreamRef test(storage);
+	std::array<std::byte, 1> buffer{};
+	ULONG data_written = -1;
+
+	REQUIRE(test.Write(buffer.data(), 0, &data_written) == S_OK);
+}
+
+TEST_CASE("When StdStreamRef.Write has an empty buffer, with a counter, the counter is zero.")
+{
+	std::stringstream storage;
+	win32::com::StdStreamRef test(storage);
+	std::array<std::byte, 1> buffer{};
+	ULONG data_written = -1;
+
+	test.Write(buffer.data(), 0, &data_written);
+	REQUIRE(data_written == 0);
+}
+
+TEST_CASE("When StdStreamRef.Write has a 16 byte buffer, with a null counter, the return code is okay.")
+{
+	std::stringstream storage;
+	win32::com::StdStreamRef test(storage);
+	std::array<std::byte, 16> buffer{};
+	std::memcpy(buffer.data(), "Hello world again", buffer.size());
+
+	REQUIRE(test.Write(buffer.data(), buffer.size(), nullptr) == S_OK);
+}
+
+TEST_CASE("When StdStreamRef.Write has a 16 byte buffer, with a null counter, the written data is correct.")
+{
+	std::stringstream storage;
+	win32::com::StdStreamRef test(storage);
+	std::array<std::byte, 16> buffer{};
+	std::memcpy(buffer.data(), "Hello world again", buffer.size());
+
+	test.Write(buffer.data(), buffer.size(), nullptr);
+
+	REQUIRE(storage.str() == "Hello world agai");
+}
+
+TEST_CASE("When StdStreamRef.Write has a 16 byte buffer, with a counter, the return code is okay.")
+{
+	std::stringstream storage;
+	win32::com::StdStreamRef test(storage);
+	std::array<std::byte, 16> buffer{};
+	std::memcpy(buffer.data(), "Hello world again", buffer.size());
+	ULONG data_written = -1;
+
+	REQUIRE(test.Write(buffer.data(), buffer.size(), &data_written) == S_OK);
+}
+
+TEST_CASE("When StdStreamRef.Write has a 16 byte buffer, with a counter, the count is correct.")
+{
+	std::stringstream storage;
+	win32::com::StdStreamRef test(storage);
+	std::array<std::byte, 16> buffer{};
+	std::memcpy(buffer.data(), "Hello world again", buffer.size());
+	ULONG data_written = -1;
+
+	test.Write(buffer.data(), buffer.size(), &data_written);
+
+	REQUIRE(data_written == buffer.size());
+}
+
+TEST_CASE("When StdStreamRef.Write has a 16 byte buffer, with a counter, the written data is correct.")
+{
+	std::stringstream storage;
+	win32::com::StdStreamRef test(storage);
+	std::array<std::byte, 16> buffer{};
+	std::memcpy(buffer.data(), "Hello world again", buffer.size());
+	ULONG data_written = -1;
+
+	test.Write(buffer.data(), buffer.size(), &data_written);
+
+	REQUIRE(storage.str() == "Hello world agai");
 }
