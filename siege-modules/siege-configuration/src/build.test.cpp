c71d039589837fb03174414698a5280c6aed2d84
Matthew Rindel <matthew@sobantu.co.za>
2024-06-05 19:25:06 +0200

    Turned several test sections into test cases for command line log readability

--- siege-modules/siege-configuration/src/build.test.cpp
+++ siege-modules/siege-configuration/src/build.test.cpp
@@ -6,25 +6,26 @@ namespace build = siege::configuration::build;
 
 using namespace std::literals;
 
-TEST_CASE("Parsing of an build config file", "[cfg.build]")
+TEST_CASE("Parsing of an build config file, with sample build data, values convert correctly", "[cfg.build]")
 {
-  SECTION("With sample build data, values convert correctly")
-  {
-    std::stringstream raw_config;
-    raw_config << "[Controls]" << "\r\n";
+  std::stringstream raw_config;
+  raw_config << "[Controls]"
+             << "\r\n";
 
-    raw_config << "JoystickButton0 = \"Fire\"" << "\r\n";
-    raw_config << "MouseAiming = 0" << "\r\n";
-    raw_config << "JoystickAnalogAxes0 = \"analog_turning\"" << "\r\n";
+  raw_config << "JoystickButton0 = \"Fire\""
+             << "\r\n";
+  raw_config << "MouseAiming = 0"
+             << "\r\n";
+  raw_config << "JoystickAnalogAxes0 = \"analog_turning\""
+             << "\r\n";
 
-    auto value = build::load_config(raw_config, raw_config.tellp());
+  auto value = build::load_config(raw_config, raw_config.tellp());
 
-    REQUIRE(value.has_value() == true);
+  REQUIRE(value.has_value() == true);
 
-    REQUIRE(value.value().keys().size() == 3);
+  REQUIRE(value.value().keys().size() == 3);
 
-    REQUIRE(value.value().find({"Controls", "JoystickButton0"}) == "Fire");
-    REQUIRE(value.value().find({"Controls", "MouseAiming"}) == "0");
-    REQUIRE(value.value().find({"Controls", "JoystickAnalogAxes0"}) == "analog_turning");
-  }
+  REQUIRE(value.value().find({ "Controls", "JoystickButton0" }) == "Fire");
+  REQUIRE(value.value().find({ "Controls", "MouseAiming" }) == "0");
+  REQUIRE(value.value().find({ "Controls", "JoystickAnalogAxes0" }) == "analog_turning");
 }
--- siege-modules/siege-configuration/src/dosbox.test.cpp
+++ siege-modules/siege-configuration/src/dosbox.test.cpp
@@ -6,37 +6,47 @@ namespace dosbox = siege::configuration::dosbox;
 
 using namespace std::literals;
 
-TEST_CASE("Parsing of an dosbox mapper config file", "[cfg.dosbox.mapper]")
+TEST_CASE("Parsing of an dosbox mapper config file, with sample mapper data, values convert correctly", "[cfg.dosbox.mapper]")
 {
-  SECTION("With sample mapper data, values convert correctly")
-  {
-    std::stringstream raw_config;
+  std::stringstream raw_config;
 
-    raw_config << "hand_shutdown \"key 290 mod1\"" << "\r\n";
-    raw_config << "key_a \"key 97\"" << "\r\n";
-    raw_config << "key_s \"key 115\"" << "\r\n";
-    raw_config << "key_d \"key 100\"" << "\r\n";
-    raw_config << "key_f \"key 102\"" << "\r\n";
-    raw_config << "j_button_0_0 \"stick_0 axis 2 0\"" << "\r\n";
-    raw_config << "j_button_0_1 \"stick_0 axis 2 1\"" << "\r\n";
-    raw_config << "jaxis_0_1-" << "\r\n";
-    raw_config << "jaxis_0_1+" << "\r\n";
-    raw_config << "mod_1 \"key 305\" \"key 306\"" << "\r\n";
-    raw_config << "mod_2 \"key 307\" \"key 308\"" << "\r\n";
-    raw_config << "mod_3" << "\r\n";
+  raw_config << "hand_shutdown \"key 290 mod1\""
+             << "\r\n";
+  raw_config << "key_a \"key 97\""
+             << "\r\n";
+  raw_config << "key_s \"key 115\""
+             << "\r\n";
+  raw_config << "key_d \"key 100\""
+             << "\r\n";
+  raw_config << "key_f \"key 102\""
+             << "\r\n";
+  raw_config << "j_button_0_0 \"stick_0 axis 2 0\""
+             << "\r\n";
+  raw_config << "j_button_0_1 \"stick_0 axis 2 1\""
+             << "\r\n";
+  raw_config << "jaxis_0_1-"
+             << "\r\n";
+  raw_config << "jaxis_0_1+"
+             << "\r\n";
+  raw_config << "mod_1 \"key 305\" \"key 306\""
+             << "\r\n";
+  raw_config << "mod_2 \"key 307\" \"key 308\""
+             << "\r\n";
+  raw_config << "mod_3"
+             << "\r\n";
 
-    auto value = dosbox::mapper::load_config(raw_config, raw_config.tellp());
+  auto value = dosbox::mapper::load_config(raw_config, raw_config.tellp());
 
-    REQUIRE(value.has_value() == true);
+  REQUIRE(value.has_value() == true);
 
-    REQUIRE(value.value().keys().size() == 12);
+  REQUIRE(value.value().keys().size() == 12);
 
-    REQUIRE(value.value().find("hand_shutdown"sv) == "key 290 mod1");
+  REQUIRE(value.value().find("hand_shutdown"sv) == "key 290 mod1");
   //  REQUIRE(value.value().find("key_a"sv) == "key 97");
   //  REQUIRE(value.value().find("key_s"sv) == "key 115");
- //   REQUIRE(value.value().find("key_d"sv) == "key 100");
- //   REQUIRE(value.value().find("key_f"sv) == "key 102");
- //   REQUIRE(value.value().find("j_button_0_0"sv) == "stick_0 axis 2 0");
+  //   REQUIRE(value.value().find("key_d"sv) == "key 100");
+  //   REQUIRE(value.value().find("key_f"sv) == "key 102");
+  //   REQUIRE(value.value().find("j_button_0_0"sv) == "stick_0 axis 2 0");
   //  REQUIRE(value.value().find("j_button_0_1"sv) == "stick_0 axis 2 1");
   //  REQUIRE(value.value().find("jaxis_0_1-"sv) == "");
   //  REQUIRE(value.value().find("jaxis_0_1+"sv) == "");
@@ -46,5 +56,4 @@ TEST_CASE("Parsing of an dosbox mapper config file", "[cfg.dosbox.mapper]")
   //  REQUIRE(value.value().find("mod_2"sv).at(0) == "key 307");
   //  REQUIRE(value.value().find("mod_2"sv).at(1) == "key 308");
   //  REQUIRE(value.value().find("mod_3"sv) == "");
-  }
 }
