0fca927e02450a75090bac6aa3885adc741a6fa9
Matthew Rindel <matthew@sobantu.co.za>
2024-06-06 05:50:00 +0200

    Additional enumerator type tests

--- siege-modules/siege-platform/src/com-enumerable-tests.cpp
+++ siege-modules/siege-platform/src/com-enumerable-tests.cpp
@@ -77,7 +77,7 @@ TEST_CASE("When RangeEnumerator<Variant>.QueryInterface has IEnumString, return 
   REQUIRE(test->QueryInterface(IID_IEnumString, temp.put_void()) == E_NOINTERFACE);
 }
 
-TEST_CASE("When RangeEnumerator<std::wstring_view>.QueryInterface has IEnumString, return code is okay.")
+TEST_CASE("When RangeEnumerator<wstring_view>.QueryInterface has IEnumString, return code is okay.")
 {
   std::array<std::wstring_view, 1> storage{};
   auto test = win32::com::make_unique_range_enumerator<std::wstring_view>(storage.begin(), storage.begin(), storage.end());
@@ -87,10 +87,100 @@ TEST_CASE("When RangeEnumerator<std::wstring_view>.QueryInterface has IEnumStrin
   REQUIRE(test->QueryInterface(IID_IEnumString, temp.put_void()) == S_OK);
 }
 
-TEST_CASE("When RangeEnumerator<std::wstring_view>.QueryInterface has IEnumVARIANT, return code is no interface.")
+TEST_CASE("When RangeEnumerator<wstring_view>.QueryInterface has IEnumVARIANT, return code is no interface.")
 {
   std::array<std::wstring_view, 1> storage{};
   auto test = win32::com::make_unique_range_enumerator<std::wstring_view>(storage.begin(), storage.begin(), storage.end());
+
+  win32::com::com_ptr<IEnumVARIANT> temp;
+
+  REQUIRE(test->QueryInterface(IID_IEnumVARIANT, temp.put_void()) == E_NOINTERFACE);
+}
+
+TEST_CASE("When RangeEnumerator<com_ptr<IUnknown>>.QueryInterface has IEnumUnknown, return code is okay.")
+{
+  std::array<win32::com::com_ptr<IUnknown>, 1> storage{};
+  auto test = win32::com::make_unique_range_enumerator<win32::com::com_ptr<IUnknown>>(storage.begin(), storage.begin(), storage.end());
+
+  win32::com::com_ptr<IEnumUnknown> temp;
+
+  REQUIRE(test->QueryInterface(IID_IEnumUnknown, temp.put_void()) == S_OK);
+}
+
+TEST_CASE("When RangeEnumerator<com_ptr<IUnknown>>.QueryInterface has IEnumVARIANT, return code is no interface.")
+{
+  std::array<win32::com::com_ptr<IUnknown>, 1> storage{};
+  auto test = win32::com::make_unique_range_enumerator<win32::com::com_ptr<IUnknown>>(storage.begin(), storage.begin(), storage.end());
+
+  win32::com::com_ptr<IEnumVARIANT> temp;
+
+  REQUIRE(test->QueryInterface(IID_IEnumVARIANT, temp.put_void()) == E_NOINTERFACE);
+}
+
+TEST_CASE("When RangeEnumerator<com_ptr<IDispatch>>.QueryInterface has IEnumUnknown, return code is okay.")
+{
+  std::array<win32::com::com_ptr<IDispatch>, 1> storage{};
+  auto test = win32::com::make_unique_range_enumerator<win32::com::com_ptr<IDispatch>>(storage.begin(), storage.begin(), storage.end());
+
+  win32::com::com_ptr<IEnumUnknown> temp;
+
+  REQUIRE(test->QueryInterface(IID_IEnumUnknown, temp.put_void()) == S_OK);
+}
+
+TEST_CASE("When RangeEnumerator<com_ptr<IDispatch>>.QueryInterface has IEnumVARIANT, return code is no interface.")
+{
+  std::array<win32::com::com_ptr<IDispatch>, 1> storage{};
+  auto test = win32::com::make_unique_range_enumerator<win32::com::com_ptr<IDispatch>>(storage.begin(), storage.begin(), storage.end());
+
+  win32::com::com_ptr<IEnumVARIANT> temp;
+
+  REQUIRE(test->QueryInterface(IID_IEnumVARIANT, temp.put_void()) == E_NOINTERFACE);
+}
+
+TEST_CASE("When RangeEnumerator<com_ptr<IConnectionPointContainer>>.QueryInterface has IEnumUnknown, return code is okay.")
+{
+  std::array<win32::com::com_ptr<IConnectionPointContainer>, 1> storage{};
+  auto test = win32::com::make_unique_range_enumerator<win32::com::com_ptr<IConnectionPointContainer>>(storage.begin(), storage.begin(), storage.end());
+
+  win32::com::com_ptr<IEnumUnknown> temp;
+
+  REQUIRE(test->QueryInterface(IID_IEnumUnknown, temp.put_void()) == S_OK);
+}
+
+TEST_CASE("When RangeEnumerator<com_ptr<IConnectionPointContainer>>.QueryInterface has IEnumVARIANT, return code is no interface.")
+{
+  std::array<win32::com::com_ptr<IConnectionPointContainer>, 1> storage{};
+  auto test = win32::com::make_unique_range_enumerator<win32::com::com_ptr<IConnectionPointContainer>>(storage.begin(), storage.begin(), storage.end());
+
+  win32::com::com_ptr<IEnumVARIANT> temp;
+
+  REQUIRE(test->QueryInterface(IID_IEnumVARIANT, temp.put_void()) == E_NOINTERFACE);
+}
+
+TEST_CASE("When RangeEnumerator<com_ptr<IConnectionPoint>>.QueryInterface has IEnumConnectionPoints, return code is okay.")
+{
+  std::array<win32::com::com_ptr<IConnectionPoint>, 1> storage{};
+  auto test = win32::com::make_unique_range_enumerator<win32::com::com_ptr<IConnectionPoint>>(storage.begin(), storage.begin(), storage.end());
+
+  win32::com::com_ptr<IEnumUnknown> temp;
+
+  REQUIRE(test->QueryInterface(IID_IEnumConnectionPoints, temp.put_void()) == S_OK);
+}
+
+TEST_CASE("When RangeEnumerator<com_ptr<IConnectionPoint>>.QueryInterface has IEnumUnknown, return code is no interface.")
+{
+  std::array<win32::com::com_ptr<IConnectionPoint>, 1> storage{};
+  auto test = win32::com::make_unique_range_enumerator<win32::com::com_ptr<IConnectionPoint>>(storage.begin(), storage.begin(), storage.end());
+
+  win32::com::com_ptr<IEnumUnknown> temp;
+
+  REQUIRE(test->QueryInterface(IID_IEnumUnknown, temp.put_void()) == E_NOINTERFACE);
+}
+
+TEST_CASE("When RangeEnumerator<com_ptr<IConnectionPoint>>.QueryInterface has IEnumVARIANT, return code is no interface.")
+{
+  std::array<win32::com::com_ptr<IConnectionPoint>, 1> storage{};
+  auto test = win32::com::make_unique_range_enumerator<win32::com::com_ptr<IConnectionPoint>>(storage.begin(), storage.begin(), storage.end());
 
   win32::com::com_ptr<IEnumVARIANT> temp;
 
