af16ef37c166c9a060a80e1cee755299ede0e67d
Matthew Rindel <matthew@sobantu.co.za>
2024-10-20 08:42:56 +0200

    Added intial version of mask of eternity module

--- siege-modules/siege-extension-darkstar/src/moe.cpp
+++ siege-modules/siege-extension-darkstar/src/moe.cpp
@@ -11,201 +11,211 @@
 #include <siege/platform/win/desktop/window_module.hpp>
 #include <siege/platform/win/desktop/window_impl.hpp>
 #include <detours.h>
+#include "shared.hpp"
 
-extern "C"
+extern "C" {
+using namespace std::literals;
+
+constexpr std::array<std::array<std::pair<std::string_view, std::size_t>, 3>, 1> verification_strings = { { std::array<std::pair<std::string_view, std::size_t>, 3>{ { { "alias"sv, std::size_t(0x61cc48) },
+  { "quit"sv, std::size_t(0x61cbec) },
+  { "KQGame::PlayFromCD"sv, std::size_t(0x5b0170) } } } } };
+
+constexpr static std::array<std::pair<std::string_view, std::string_view>, 15> function_name_ranges{ {
+  { "KQMusic::delete"sv, "KQMusic::play"sv },
+  { "killGame"sv, "deleteObject"sv },
+  { "KQObject::setGroupsActive3DFar"sv, "KQObject::setLoc"sv },
+  { "KQCamera::setHeight"sv, "KQCamera::follow"sv },
+  { "KQGame::letCatchUp"sv, "profile"sv },
+  { "KQConner::removeFromTrap"sv, "KQMonster::god"sv },
+  { "KQEmitter::reinit"sv, "KQEmitter::setFollowObject"sv },
+  { "KQMap::stopLoc"sv, "KQMap::activate"sv },
+  { "Heap::findLeaks"sv, "Heap::check"sv },
+  { "messageCanvasDevice"sv, "swapSurfaces"sv },
+  { "newPointLight"sv, "newDirectionalLight"sv },
+  { "globeLines"sv, "newSky"sv },
+  { "loadInterior"sv, "loadInterior"sv },
+  { "quit"sv, "alias"sv },
+  { "KQSound::setMixLimit"sv, "KQSound::setSpeechVol"sv },
+} };
+
+constexpr static std::array<std::pair<std::string_view, std::string_view>, 3> variable_name_ranges{ { { "NewWorld"sv, "KQPortal::ConLoc"sv },
+  { "allowAltEnter"sv, "ConsoleWorld::FrameRate"sv },
+  { "SetFirstPerson::WasThirdPerson"sv, "ConInv::Boots"sv } } };
+
+
+static std::int32_t(__fastcall* CopyDir)(const char*, const char*, BOOL) = nullptr;
+
+inline void set_gog_exports()
 {
-#define DARKCALL __attribute__((regparm(3)))
 
-	static DARKCALL char* (*ConsoleEval)(void*, std::int32_t, std::int32_t, const char**) = nullptr;
+  CopyDir = (decltype(CopyDir))0x499520;
+}
 
-	using namespace std::literals;
+constexpr std::array<void (*)(), 1> export_functions = { {
+  set_gog_exports,
+} };
 
-	constexpr std::array<std::array<std::pair<std::string_view, std::size_t>, 4>, 1> verification_strings = { {
-	std::array<std::pair<std::string_view, std::size_t>, 3>{{
-		{"alias"sv, std::size_t(0x61cc48)},
-		{"quit"sv, std::size_t(0x61cbec)},
-		{"KQGame::PlayFromCD"sv, std::size_t(0x5b0170)}
-		}} 
-	};
+HRESULT get_function_name_ranges(std::size_t length, std::array<const char*, 2>* data, std::size_t* saved) noexcept
+{
+  return siege::get_name_ranges(function_name_ranges, length, data, saved);
+}
 
-	constexpr static std::array<std::pair<std::string_view, std::string_view>, 28> function_name_ranges{ {
-		{"KQMusic::play"sv, "KQMusic::delete"sv},
-		{"deleteObjects"sv, "killGame"sv},
-		{"KQObject::setLoc"sv, "KQObject::setGroupsActive3DFar"sv},
-		{"KQCamera::follow"sv, "KQCamera::setHeight"sv},
-		{"profile"sv, "KQGame::letCatchUp"sv},
-		{"KQMonster::god"sv, "KQConner::removeFromTrap"sv},
-		{"KQEmitter::setFollowObject"sv, "KQEmitter::reinit"sv},
-		{"KQMap::activate"sv, "KQMap::stopLoc"sv},
-		{"Heap::check"sv, "Heap::findLeaks"sv},
-		{"swapSurfaces"sv, "messageCanvasDevice"sv},
-		{"newDirectionalLight"sv, "newPointLight"sv},
-		{"newSky"sv, "globeLines"sv},
-		{"loadInterior"sv, "loadInterior"sv},
-		{"alias"sv, "quit"sv},
-		{"KQSound::setSpeechVol"sv, "KQSound::setMixLimit"sv},
-		
-		} };
+HRESULT get_variable_name_ranges(std::size_t length, std::array<const char*, 2>* data, std::size_t* saved) noexcept
+{
+  return siege::get_name_ranges(variable_name_ranges, length, data, saved);
+}
 
-	constexpr static std::array<std::pair<std::string_view, std::string_view>, 13> variable_name_ranges{ {
-		{"KQPortal::ConLoc"sv, "NewWorld"sv},
-		{"ConsoleWorld::FrameRate"sv, "allowAltEnter"sv},
-		{"SetFirstPerson::WasThirdPerson"sv, "ConInv::Boots"sv}
-		} };
+HRESULT executable_is_supported(_In_ const wchar_t* filename) noexcept
+{
+  return siege::executable_is_supported(filename, verification_strings[0], function_name_ranges, variable_name_ranges);
+}
 
-	inline void set_gog_exports()
-	{
-	}
+static auto* TrueOutputDebugStringA = OutputDebugStringA;
 
-	constexpr std::array<void(*)(), 5> export_functions = { {
-			set_gog_exports,
-		} };
+static std::ofstream mask_log("mask-detour.log", std::ios::trunc);
 
-
-	static auto* TrueSetWindowsHookExA = SetWindowsHookExA;
-
-	HHOOK WINAPI WrappedSetWindowsHookExA(int idHook, HOOKPROC lpfn, HINSTANCE hmod, DWORD dwThreadId)
-	{
-		win32::com::init_com();
-
-		if (dwThreadId == 0)
-		{
-			dwThreadId = ::GetCurrentThreadId();
-		}
-
-		return TrueSetWindowsHookExA(idHook, lpfn, hmod, dwThreadId);
-	}
-
-	static std::array<std::pair<void**, void*>, 1> detour_functions{ {
-		{ &(void*&)TrueSetWindowsHookExA, WrappedSetWindowsHookExA }
-	} };
-
-	BOOL WINAPI DllMain(
-		HINSTANCE hinstDLL,
-		DWORD fdwReason,
-		LPVOID lpvReserved) noexcept
-	{
-		if (DetourIsHelperProcess())
-		{
-			return TRUE;
-		}
-
-		if (fdwReason == DLL_PROCESS_ATTACH || fdwReason == DLL_PROCESS_DETACH)
-		{
-			auto app_module = win32::module_ref(::GetModuleHandleW(nullptr));
-
-			auto value = app_module.GetProcAddress<std::uint32_t*>("DisableSiegeExtensionModule");
-
-			if (value && *value == -1)
-			{
-				return TRUE;
-			}
-		}
-
-		if (fdwReason == DLL_PROCESS_ATTACH || fdwReason == DLL_PROCESS_DETACH)
-		{
-			if (fdwReason == DLL_PROCESS_ATTACH)
-			{
-				int index = 0;
-				try
-				{
-					auto app_module = win32::module_ref(::GetModuleHandleW(nullptr));
-
-					std::unordered_set<std::string_view> functions;
-					std::unordered_set<std::string_view> variables;
-
-					bool module_is_valid = false;
-
-					for (const auto& item : verification_strings)
-					{
-						win32::module_ref temp((void*)item[0].second);
-
-						if (temp != app_module)
-						{
-							continue;
-						}
-
-						module_is_valid = std::all_of(item.begin(), item.end(), [](const auto& str) {
-							return std::memcmp(str.first.data(), (void*)str.second, str.first.size()) == 0;
-							});
-
-
-						if (module_is_valid)
-						{
-							export_functions[index]();
-
-							std::string_view string_section((const char*)ConsoleEval, 1024 * 1024 * 2);
-
-
-							for (auto& pair : function_name_ranges)
-							{
-								auto first_index = string_section.find(pair.first.data(), 0, pair.first.size() + 1);
-
-								if (first_index != std::string_view::npos)
-								{
-									auto second_index = string_section.find(pair.second.data(), first_index, pair.second.size() + 1);
-
-									if (second_index != std::string_view::npos)
-									{
-										auto second_ptr = string_section.data() + second_index;
-										auto end = second_ptr + std::strlen(second_ptr) + 1;
-
-										for (auto start = string_section.data() + first_index; start != end; start += std::strlen(start) + 1)
-										{
-											std::string_view temp(start);
-
-											if (temp.size() == 1)
-											{
-												continue;
-											}
-
-											if (!std::all_of(temp.begin(), temp.end(), [](auto c) { return std::isalnum(c) != 0; }))
-											{
-												break;
-											}
-
-											functions.emplace(temp);
-										}
-									}
-								}
-							}
-
-							break;
-						}
-						index++;
-					}
-
-					if (!module_is_valid)
-					{
-						return FALSE;
-					}
-
-					DetourRestoreAfterWith();
-
-					DetourTransactionBegin();
-					DetourUpdateThread(GetCurrentThread());
-
-					std::for_each(detour_functions.begin(), detour_functions.end(), [](auto& func) { DetourAttach(func.first, func.second); });
-
-					DetourTransactionCommit();
-				}
-				catch (...)
-				{
-					return FALSE;
-				}
-			}
-			else if (fdwReason == DLL_PROCESS_DETACH)
-			{
-				DetourTransactionBegin();
-				DetourUpdateThread(GetCurrentThread());
-
-				std::for_each(detour_functions.begin(), detour_functions.end(), [](auto& func) { DetourDetach(func.first, func.second); });
-				DetourTransactionCommit();
-			}
-		}
-
-		return TRUE;
-	}
+void __stdcall WrappedTrueOutputDebugStringA(const char* message)
+{
+  //  mask_log << "Debug: " << message << std::endl;
+  return TrueOutputDebugStringA(message);
 }
 
 
+LRESULT CALLBACK hook_proc(int code, WPARAM wParam, LPARAM lParam)
+{
+  if (code == HC_ACTION)
+  {
+
+    CopyDir(".\\data\\Barren\\8bit.vol", ".\\Barren\\8bit\\", TRUE);
+    CopyDir(".\\data\\Barren\\8gui.vol", ".\\Barren\\8gui\\", TRUE);
+    CopyDir(".\\data\\Barren\\kq.vol", ".\\Barren\\kq\\", TRUE);
+    CopyDir(".\\data\\Barren\\resource.vol", ".\\Barren\\resource\\", TRUE);
+
+    CopyDir(".\\data\\daventry\\8bit.vol", ".\\daventry\\8bit\\", TRUE);
+    CopyDir(".\\data\\daventry\\8gui.vol", ".\\daventry\\8gui\\", TRUE);
+    CopyDir(".\\data\\daventry\\kq.vol", ".\\daventry\\kq\\", TRUE);
+    CopyDir(".\\data\\daventry\\resource.vol", ".\\daventry\\resource\\", TRUE);
+
+    CopyDir(".\\data\\deadcity\\8bit.vol", ".\\deadcity\\8bit\\", TRUE);
+    CopyDir(".\\data\\deadcity\\8gui.vol", ".\\deadcity\\8gui\\", TRUE);
+    CopyDir(".\\data\\deadcity\\kq.vol", ".\\deadcity\\kq\\", TRUE);
+    CopyDir(".\\data\\deadcity\\resource.vol", ".\\deadcity\\resource\\", TRUE);
+
+    CopyDir(".\\data\\iceworld\\8bit.vol", ".\\iceworld\\8bit\\", TRUE);
+    CopyDir(".\\data\\iceworld\\8gui.vol", ".\\iceworld\\8gui\\", TRUE);
+    CopyDir(".\\data\\iceworld\\kq.vol", ".\\iceworld\\kq\\", TRUE);
+    CopyDir(".\\data\\iceworld\\resource.vol", ".\\iceworld\\resource\\", TRUE);
+
+    CopyDir(".\\data\\temple1\\8bit.vol", ".\\temple1\\8bit\\", TRUE);
+    CopyDir(".\\data\\temple1\\8gui.vol", ".\\temple1\\8gui\\", TRUE);
+    CopyDir(".\\data\\temple1\\kq.vol", ".\\temple1\\kq\\", TRUE);
+    CopyDir(".\\data\\temple1\\resource.vol", ".\\temple1\\resource\\", TRUE);
+
+    CopyDir(".\\data\\temple2\\8bit.vol", ".\\temple2\\8bit\\", TRUE);
+    CopyDir(".\\data\\temple2\\8gui.vol", ".\\temple2\\8gui\\", TRUE);
+    CopyDir(".\\data\\temple2\\kq.vol", ".\\temple2\\kq\\", TRUE);
+    CopyDir(".\\data\\temple2\\resource.vol", ".\\temple2\\resource\\", TRUE);
+
+    CopyDir(".\\data\\temple3\\8bit.vol", ".\\temple3\\8bit\\", TRUE);
+    CopyDir(".\\data\\temple3\\8gui.vol", ".\\temple3\\8gui\\", TRUE);
+    CopyDir(".\\data\\temple3\\kq.vol", ".\\temple3\\kq\\", TRUE);
+    CopyDir(".\\data\\temple3\\resource.vol", ".\\temple3\\resource\\", TRUE);
+    ExitProcess(0);
+  }
+
+  return CallNextHookEx(nullptr, code, wParam, lParam);
+}
+
+
+static std::array<std::pair<void**, void*>, 1> detour_functions{ { { &(void*&)TrueOutputDebugStringA, WrappedTrueOutputDebugStringA } } };
+
+BOOL WINAPI DllMain(
+  HINSTANCE hinstDLL,
+  DWORD fdwReason,
+  LPVOID lpvReserved) noexcept
+{
+  if (DetourIsHelperProcess())
+  {
+    return TRUE;
+  }
+
+  if (fdwReason == DLL_PROCESS_ATTACH || fdwReason == DLL_PROCESS_DETACH)
+  {
+    auto app_module = win32::module_ref(::GetModuleHandleW(nullptr));
+
+    auto value = app_module.GetProcAddress<std::uint32_t*>("DisableSiegeExtensionModule");
+
+    if (value && *value == -1)
+    {
+      return TRUE;
+    }
+  }
+
+  if (fdwReason == DLL_PROCESS_ATTACH || fdwReason == DLL_PROCESS_DETACH)
+  {
+    if (fdwReason == DLL_PROCESS_ATTACH)
+    {
+      int index = 0;
+      try
+      {
+        auto app_module = win32::module_ref(::GetModuleHandleW(nullptr));
+
+        bool module_is_valid = false;
+
+        for (const auto& item : verification_strings)
+        {
+          win32::module_ref temp((void*)item[0].second);
+
+          if (temp != app_module)
+          {
+            continue;
+          }
+
+          module_is_valid = std::all_of(item.begin(), item.end(), [](const auto& str) {
+            return std::memcmp(str.first.data(), (void*)str.second, str.first.size()) == 0;
+          });
+
+
+          if (module_is_valid)
+          {
+            export_functions[index]();
+            break;
+          }
+          index++;
+        }
+
+        if (!module_is_valid)
+        {
+          return FALSE;
+        }
+
+        DetourRestoreAfterWith();
+
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+
+        std::for_each(detour_functions.begin(), detour_functions.end(), [](auto& func) { DetourAttach(func.first, func.second); });
+
+        DetourTransactionCommit();
+
+
+        SetWindowsHookExA(WH_GETMESSAGE, hook_proc, hinstDLL, ::GetCurrentThreadId());
+      }
+      catch (...)
+      {
+        return FALSE;
+      }
+    }
+    else if (fdwReason == DLL_PROCESS_DETACH)
+    {
+      DetourTransactionBegin();
+      DetourUpdateThread(GetCurrentThread());
+
+      std::for_each(detour_functions.begin(), detour_functions.end(), [](auto& func) { DetourDetach(func.first, func.second); });
+      DetourTransactionCommit();
+    }
+  }
+
+  return TRUE;
+}
+}
