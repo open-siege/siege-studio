28c7125254807aa9ebdb0c6f42454ef316ed29f4
Matthew Rindel <matthew@sobantu.co.za>
2025-06-05 05:43:01 +0200

    Added initial console processing code to game unpack

--- siege-tools/game-unpack/src/game-unpack.win32.cpp
+++ siege-tools/game-unpack/src/game-unpack.win32.cpp
@@ -5,18 +5,33 @@
 #include <siege/platform/extension_module.hpp>
 #include <span>
 #include <string_view>
+#include <array>
 #include <ranges>
 #include <filesystem>
 
 namespace fs = std::filesystem;
 
+bool is_standalone_console()
+{
+  std::array<DWORD, 4> ids{};
+
+  return GetConsoleProcessList(ids.data(), (DWORD)ids.size()) == 1;
+}
+
 int main(int argc, const char* argv[])
 {
   auto args = std::span(argv, argc) | std::views::transform([](char const* v) { return std::string_view(v); });
 
-  
-
   auto installation_modules = siege::platform::installation_module::load_modules(fs::current_path());
+
+  if (args.size() == 1 && is_standalone_console())
+  {
+    ::ShowWindow(::GetConsoleWindow(), SWP_HIDEWINDOW);
+    // TODO create task dialog window
+  }
+
+
+
 
   return 0;
 }
