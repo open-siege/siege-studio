e5b964c67ddcc2d52b69568cbd4b969e61606c07
Matthew Rindel <matthew@sobantu.co.za>
2025-07-11 17:03:26 +0200

    Fix for clamp when resizing window

--- siege-studio/src/views/siege_main_window.hpp
+++ siege-studio/src/views/siege_main_window.hpp
@@ -675,7 +675,7 @@ namespace siege::views
         total_width = total_width - child_width;
       }
 
-      auto width = std::clamp<int>(total_width / count, fallback, right_size.cx - 25);// TODO figure out the correct sizing for the tab control
+      auto width = std::clamp<int>(total_width / count, fallback, right_size.cx > 25 ? right_size.cx - 25 : 0);// TODO figure out the correct sizing for the tab control
 
       TabCtrl_SetPadding(tab_control, 10, 0);
       auto old_height = HIWORD(TabCtrl_SetItemSize(tab_control, width, 40));
