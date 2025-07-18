f0e01d8f5b92124558bc98cf566f799642faffc1
Matthew Rindel <matthew@sobantu.co.za>
2025-07-18 16:18:48 +0200

    Added a setting for listen for quake 3 and fakk2

--- siege-modules/extension/siege-extension-id-tech/src/fakk-2.cpp
+++ siege-modules/extension/siege-extension-id-tech/src/fakk-2.cpp
@@ -27,10 +27,14 @@ using predefined_int = siege::platform::game_command_line_predefined_setting<int
 using predefined_string = siege::platform::game_command_line_predefined_setting<const wchar_t*>;
 
 extern auto command_line_caps = game_command_line_caps{
-  .int_settings = { { L"r_customwidth", L"r_customheight", L"r_mode" } },
+  .int_settings = { { L"r_customwidth", L"r_customheight", L"r_mode", L"net_noipx" } },
   .string_settings = { { L"name", L"connect", L"map", L"r_glDriver" } },
   .ip_connect_setting = L"connect",
   .player_name_setting = L"name",
+  // not actually the setting because the game starts a listen server by default
+  // by this forces the UI to have the "listen" option.
+  // TODO look into a better way of representing this
+  .listen_setting = L"net_noipx", 
 };
 
 extern auto game_actions = std::array<game_action, 32>{ {
