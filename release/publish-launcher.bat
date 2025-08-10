3f1333f02c1ef569c1d4a75fa2bcfb52f98edb5a
Matthew Rindel <matthew@sobantu.co.za>
2025-08-10 06:18:26 +0200

    Added publish scripts for itch.io

---
+++ release/publish-launcher.bat
@@ -0,0 +1,1 @@
+butler push build\siege-launcher\Release\siege-launcher.exe thesiegehub/siege-launcher:development --userversion %1
---
+++ release/publish-studio.bat
@@ -0,0 +1,1 @@
+butler push build\siege-studio\Release\siege-studio.exe thesiegehub/siege-studio:development --userversion %1 
