934601f386c89266fddda6b8f5ec2061759c9438
Matthew Rindel <matthew@sobantu.co.za>
2025-08-21 07:23:19 +0200

    Removed empty projects from use
    Separated content-other into mw2, mw4 and sword

--- siege-modules/presentation/siege-presentation-configuration/src/views/cfg_controller.cpp
+++ siege-modules/presentation/siege-presentation-configuration/src/views/cfg_controller.cpp
@@ -3,9 +3,6 @@
 #include <algorithm>
 #include <istream>
 #include <siege/configuration/id_tech.hpp>
-#include <siege/configuration/unreal.hpp>
-#include <siege/configuration/build.hpp>
-#include <siege/configuration/serious.hpp>
 #include <siege/platform/stream.hpp>
 #include "cfg_controller.hpp"
 
@@ -19,14 +16,10 @@ namespace siege::views
   std::size_t cfg_controller::load_config(std::istream& stream) noexcept
   {
     using namespace siege::configuration::id_tech;
-    using namespace siege::configuration::unreal;
-    using namespace siege::configuration::serious;
-
+    
     auto size = siege::platform::get_stream_size(stream);
 
-    auto result = id_tech_2::load_config(stream, size)
-                    .or_else([&] { return unreal_1::load_config(stream, size); })
-                    .or_else([&] { return serious_1::load_config(stream, size); });
+    auto result = id_tech_2::load_config(stream, size);
 
     if (!result)
     {
