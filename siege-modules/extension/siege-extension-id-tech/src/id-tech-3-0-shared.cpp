969f2cf520495572e7f8deb2c39456502d0aa072
Matthew Rindel <matthew@sobantu.co.za>
2026-04-05 14:33:32 +0200

    Merged id tech cmd processing into shared file

--- siege-modules/extension/siege-extension-id-tech/src/id-tech-3-0-shared.cpp
+++ siege-modules/extension/siege-extension-id-tech/src/id-tech-3-0-shared.cpp
@@ -202,4 +202,112 @@ std::errc is_moh_input_mapping_valid(const siege::platform::hardware_context_cap
 
   return std::errc{};
 }
+
+using game_command_line_caps = siege::platform::game_command_line_caps;
+namespace fs = std::filesystem;
+using namespace std::literals;
+extern game_command_line_caps command_line_caps;
+
+// TODO
+// Specify controller cfg in the command line
+const wchar_t** format_command_line(const siege::platform::game_command_line_args* args, std::uint32_t* new_size)
+{
+  if (!args)
+  {
+    return nullptr;
+  }
+
+  if (!new_size)
+  {
+    return nullptr;
+  }
+
+  static std::vector<std::wstring> string_args;
+  string_args.clear();
+
+  for (auto& setting : args->string_settings)
+  {
+    if (!setting.name)
+    {
+      continue;
+    }
+
+    if (!setting.value)
+    {
+      continue;
+    }
+
+    if (!setting.value[0])
+    {
+      continue;
+    }
+
+    if (std::wstring_view(setting.name) == command_line_caps.ip_connect_setting)
+    {
+      string_args.emplace_back(L"+connect");
+      string_args.emplace_back(setting.value);
+    }
+    else if (std::wstring_view(setting.name) == L"map")
+    {
+      string_args.emplace_back(L"+map");
+      string_args.emplace_back(setting.value);
+    }
+    else if (std::wstring_view(setting.name) == L"exec")
+    {
+      string_args.emplace_back(L"+exec");
+      string_args.emplace_back(setting.value);
+    }
+    else
+    {
+      string_args.emplace_back(L"+set");
+      string_args.emplace_back(setting.name);
+      string_args.emplace_back(setting.value);
+    }
+  }
+
+  for (auto& setting : args->int_settings)
+  {
+    if (!setting.name)
+    {
+      continue;
+    }
+
+    try
+    {
+      auto name_str = std::wstring_view(setting.name);
+
+      string_args.emplace_back(L"+set");
+      string_args.emplace_back(name_str);
+      string_args.emplace_back(std::to_wstring(setting.value));
+    }
+    catch (...)
+    {
+    }
+  }
+
+  for (auto& flag : args->flags)
+  {
+    if (!flag)
+    {
+      continue;
+    }
+
+    string_args.emplace_back(flag);
+  }
+
+  if (string_args.empty())
+  {
+    return nullptr;
+  }
+
+  static std::vector<const wchar_t*> raw_args;
+  raw_args.resize(string_args.size());
+  *new_size = (std::uint32_t)string_args.size();
+
+  std::transform(string_args.begin(), string_args.end(), raw_args.begin(), [](const std::wstring& value) {
+    return value.c_str();
+  });
+
+  return raw_args.data();
+}
 }
